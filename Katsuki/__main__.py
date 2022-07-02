361234e0fd89edeec8ac857967a35df8765fa934
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 22:16:03 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -1,5 +1,8 @@
 from Katsuki import Katsuki
+import config 
 
 
 if __name__ == "__main__":
    Katsuki.start()
+   with Katsuki:
+        Katsuki.send_message(f"@{config.SUPPORT_CHAT}", "**Katsuki is Ready!**")
