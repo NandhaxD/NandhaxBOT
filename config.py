266272fae4b30ceacacf6acabc93e70f5cb3fc9d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 23:22:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class config(object):
   API_ID = os.environ.get("API_ID", None)
   API_HASH = os.environ.get("API_HASH", None)
   SESSION = os.environ.get("SESSION", None) 
-  ALIVE_NAME = os.enviorn.get("ALIVE_NAME", "𝙆𝘼𝙏𝙎𝙐𝙆𝙄")
   OWNER_USERNAME = os.enviorn.get("OWNER_USERNAME", None)
   USER = [5555573352, 1491497760, 2006619406]
   HANDLER = ["~","."]
