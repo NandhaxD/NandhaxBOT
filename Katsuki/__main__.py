1eb5d67abdb773308ea7ab089efa0a01b7e59443
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 23:51:06 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -1,8 +1,9 @@
-from Katsuki import Katsuki
-import config 
+from Katsuki import Katsuki as bot
 import Katsuki.plugins
 
 
 if __name__ == "__main__":
-   Katsuki.start()
-   
+   bot.start()
+   with bot:
+        bot.send_message(-1001696319819, "Hello there I'm Now online")
+
