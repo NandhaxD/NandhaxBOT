ed7e3983b5ee2dc17fb7aac7faec19510bc46d8b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 00:06:37 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -1,7 +1,11 @@
+import config
 from Katsuki import Katsuki as bot
 import Katsuki.plugins
 
 
 if __name__ == "__main__":
-   bot.start()
-   
+   katsuki.start()
+   bot.run()
+   with bot:
+            bot.send_message(f"{config.LOG_GROUP_ID}", "Katsuki Ready Boi!")
+
