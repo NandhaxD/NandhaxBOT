fae96c239cf048a69875ccb25a221dec8d6afb8e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 22:32:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
 class config(object):
   
   LOG_GROUP_ID = -1001696319819
+  API_ID = os.environ.get("API_ID", None)
+  API_HASH = os.environ.get("API_HASH", None)
+  SESSION = os.environ.get("SESSION", None) 
