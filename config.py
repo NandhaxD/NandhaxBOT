b3528cefa652ac4281b764b9d7139b750156bfb5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 23:31:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-import os
-
 class config(object):
   
   LOG_GROUP_ID = -1001696319819
-  API_ID = os.environ.get("API_ID", None)
-  API_HASH = os.environ.get("API_HASH", None)
-  SESSION = os.environ.get("SESSION", None) 
   USER = [5555573352, 1491497760, 2006619406]
   HANDLER = ["~","."]
