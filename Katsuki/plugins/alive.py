34f31dd26c5db91aaea7caf7bd6b21215bba2c84
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 00:16:05 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -1,7 +1,7 @@
-import config
+from config import HANDLER
 from pyrogram import filters 
 from Katsuki import katsuki
 
-@katsuki.on_message(filters.command("alive",prefixes=(config.HANDLER) & filters.user(config.USER))
+@katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(config.USER))
 async def alive(_, m):
       await m.reply_text("Hey sir I'm alive")
