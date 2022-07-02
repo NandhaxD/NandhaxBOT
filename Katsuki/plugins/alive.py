46e6d42c9d4355b1e6140665c3c82db428d57074
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <98509058+Legend-Mukund@users.noreply.github.com>
2022-07-02 22:49:12 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -2,4 +2,6 @@ import config
 from pyrogram import filters 
 from Katsuki import Katsuki
 
-do you best here and add config.py USER for only work 
+# do you best here and add config.py USER for only work 
+
+# My mom forced me to sleep bro :(
