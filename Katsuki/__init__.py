dd0dd781df4c954632ee62758b1b5f0e0a9011c1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-02 22:07:09 +0530

    Update __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -1,5 +1,15 @@
 from pyrogram import Client
 
+# enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO)
+
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+SESSION = os.environ.get("SESSION", None) 
 
 
-ubot = Client(session_string=SESSION, api_id=UB_API_ID, api_hash=UB_API_HASH, name="Katsuki")
+Kasuki = Client(session_string=SESSION, api_id=UB_API_ID, api_hash=UB_API_HASH, name="Katsuki")
