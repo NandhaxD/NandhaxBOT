bb7c08f5ca69826dde4db7b7d9398188c68ea813
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 00:02:22 +0530

    Update __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -14,5 +14,7 @@ logging.basicConfig(
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION", None) 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
 
+bot = Client("KatsukiBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 Katsuki = Client(session_string=SESSION, api_id=API_ID, api_hash=API_HASH, name="Katsuki")
