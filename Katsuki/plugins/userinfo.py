8880131ade7d6716f97f9f729dd178b7fb08aefe
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 01:12:52 +0530

    Create userinfo.py

---
+++ Katsuki/plugins/userinfo.py
@@ -0,0 +1,35 @@
+from Katsuki import katsuki 
+from pyrogram import filters 
+from config import HANDLER, OWNER_ID
+
+
+@katsuki.on_message(filters.command("id",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def id(_, m):
+         reply = m.reply_to_message
+         if not reply:
+               text = f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               text += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               text += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`"
+              await m.reply_text(text=(text)) 
+         elif not reply.sticker or reply.animation:
+               text = f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               text += f"**ʀᴇᴘʟɪᴇᴅ ɪᴅ**: `{reply.from_user.id}`\n\n"
+               text += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               text += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`"
+               await m.reply_text(text=(text))
+               return 
+         elif reply.sticker:
+               text = f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               text += f"**ʀᴇᴘʟɪᴇᴅ ɪᴅ**: `{reply.from_user.id}`\n\n"
+               text += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               text += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`\n\n\n"
+               text += f"**sᴛɪᴄᴋᴇʀ ɪᴅ**: `{reply.sticker.file_id}`"
+               await m.reply_text(text=(text)) 
+               return 
+         elif reply.animation:
+               text = f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               text += f"**ʀᴇᴘʟɪᴇᴅ ɪᴅ**: `{reply.from_user.id}`\n\n"
+               text += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               text += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`\n\n\n"
+               text += f"**ɢɪғ ɪᴅ**: `{reply.animation.file_id}`"
+               await m.reply_text(text=(text)) 
