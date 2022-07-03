f5e1735344bb10d600351a88dd7583aad791a1e0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 11:29:41 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -1,8 +1,8 @@
 import random 
-from config import HANDLER, OWNER_ID, KATSUKI
+from config import HANDLER, OWNER_ID, KATSUKI, ALIVE_TEXT
 from pyrogram import filters 
 from Katsuki import katsuki
 
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def alive(_, m):
-      await m.reply_photo(random.choice(KATSUKI))
+      await m.reply_photo(random.choice(KATSUKI)caption=ALIVE_TEXT)
