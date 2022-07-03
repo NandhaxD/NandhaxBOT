6b8df4fc3c59427ee805d86eff760cc5e1de0159
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <98509058+Legend-Mukund@users.noreply.github.com>
2022-07-03 12:00:00 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -1,10 +1,12 @@
 import random 
 import asyncio
 from config import HANDLER, OWNER_ID, KATSUKI, ALIVE_TEXT
-from pyrogram import filters
+from pyrogram import filters, __version__ as lol
 from pyrogram.types import Message
-from Katsuki import katsuki
+from Katsuki import katsuki, you
 from platform import python_version as pyver
+
+___version___ = "0.1.0"
       
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def restart(client, m: Message):
@@ -21,6 +23,20 @@ async def restart(client, m: Message):
     await asyncio.sleep(0.1)
     await m.reply_photo(
         random.choice(KATSUKI),
-        caption=ALIVE_TEXT,
+        caption=f"""
+        ━━━━━━━━━━━━━━━━━━━
+        ► **ʏᴀᴀ ᴀᴍ ᴀʟɪᴠᴇ ʀᴇᴀᴅʏ ᴛᴏ ʀᴏᴄᴋ**
+        
+        ► **ᴏᴡɴᴇʀ :** [{you.first_name}](https://t.me/{you.id})
+        
+        ► **ᴍᴇᴛᴀ ᴠᴇʀsɪᴏɴ :** `{___version___}`
+        
+        ► **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{lol}`
+        
+        ► **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{pyver()}`
+        
+        **[sᴜᴘᴘᴏʀᴛ](https://t.me/Katsuki_Support)|[ᴜᴘᴅᴀᴛᴇs](https://t.me/Katsuki_Updates)**
+        ━━━━━━━━━━━━━━━━━
+        """,
     )      
       
