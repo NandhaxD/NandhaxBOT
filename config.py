27c8e3742ae92286c3e1e73600c89d200507dbca
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 11:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 LOG_GROUP_ID = -1001696319819
 OWNER_ID = [5555573352, 1491497760, 2006619406]
-HANDLER = ["~", "."]
+HANDLER = ["~", ".",","]
+KATSUKI = [ 
+"https://telegra.ph/file/ceb6116f965695779c287.jpg",
+"https://telegra.ph/file/ccddf45a1243f807722ee.jpg",
+"https://telegra.ph/file/b12d1650d1ded880867ae.jpg",
+"https://telegra.ph/file/6a67b0cca24a8bf205637.jpg",
+"https://telegra.ph/file/01aac72ece2da9489eb9f.jpg" ]
