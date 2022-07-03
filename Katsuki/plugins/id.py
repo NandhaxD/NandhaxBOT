6b26a93b7a7502534d96a8a0ae3dc7ebb03cdea6
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 10:24:54 +0530

    Update id.py

--- Katsuki/plugins/id.py
+++ Katsuki/plugins/id.py
@@ -1,45 +1,31 @@
 from Katsuki import katsuki 
 from pyrogram import filters 
-from pyrogram.types import Message
 from config import HANDLER, OWNER_ID
 
 
-def get_id(msg: Message):
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            "sticker",
-        ):
-            obj = getattr(msg, message_type)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
 
 
 @katsuki.on_message(filters.command("id",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def id(_, m):
          reply = m.reply_to_message
-         _id = ""
+         _reply = ""
          if not reply:
-               _id += f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
-               _id += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
-               _id += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`\n\n"
-               await reply.reply_text(text=(_id))
+               no_reply = f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               no_reply += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               no_reply += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`"
+               await reply.reply_text(text=(no_reply)) 
          if reply:
-               _id += f"**ʀᴇᴩʟɪᴇᴅ ᴜsᴇʀ ɪᴅ**:\n\n"
-               file_info = get_id(m.reply_to_message)
-         elif file_info:
-                   _id += f"**{file_info.message_type}**:\n{file_info.file_id}"
-         await m.reply_text(_id) 
+               _reply += f"**ʏᴏᴜʀ ɪᴅ**: `{m.from_user.id}`\n\n"
+               _reply += f"**ʀᴇᴘʟɪᴇᴅ ɪᴅ**: `{reply.from_user.id}`\n\n"
+               _reply += f"**ᴄʜᴀᴛ ɪᴅ**: `{m.chat.id}`\n\n"
+               _reply += f"**ᴍᴇssᴀɢᴇ ɪᴅ**: `{m.id}`"
+         elif reply.sticker:
+                _reply += f"**sᴛɪᴄᴋᴇʀ ɪᴅ**: `{reply.sticker.file_id}`"
+         await m.reply_text(_reply)
                    
+
          
 
 
 
-#twst
+#test
