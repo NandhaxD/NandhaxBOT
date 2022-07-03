d7169192c975f05a9cf2529d1e8b1918eec494ac
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 11:35:23 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -5,4 +5,4 @@ from Katsuki import katsuki
 
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def alive(_, m):
-      await m.reply_photo(random.choice(KATSUKI)caption=ALIVE_TEXT)
+      await m.reply_photo(random.choice(KATSUKI),caption=ALIVE_TEXT)
