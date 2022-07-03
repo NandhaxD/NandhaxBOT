cc33eedf0900a74468f3f7e23fee4a936c5afff9
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 11:44:28 +0530

    Create __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -1,5 +1,6 @@
 import config
 import os 
+import time
 import logging
 from pyrogram import Client
 
@@ -10,6 +11,30 @@ logging.basicConfig(
               logging.StreamHandler()],
     level=logging.INFO)
 
+#bot =  [uptime,starttime,endtime]
+
+StartTime = time.time()
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+                  
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
@@ -18,4 +43,3 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 bot = Client("KatsukiBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 katsuki = Client(session_string=SESSION, api_id=API_ID, api_hash=API_HASH, name="Katsuki")
-you = katsuki.get_me()
