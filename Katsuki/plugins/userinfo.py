75bac0d6ca8e1236cc8027b7774f10cb410b6ea2
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 14:05:18 +0530

    Update userinfo.py

--- Katsuki/plugins/userinfo.py
+++ Katsuki/plugins/userinfo.py
@@ -1,5 +1,6 @@
 from config import OWNER_ID, HANDLER
-from Katsuki import bot, katsuki
+from Katsuki import katsuki
+from Katsuki.utils import section
 from pyrogram import filters 
 
 @katsuki.on_message(filters.command("cinfo",prefixes=HANDLER) & filters.user(OWNER_ID))
@@ -37,3 +38,57 @@ async def cinfo(_, m):
             text += f"✪ **MENTION:** [link](t.me/{username})"
             await katsuki.send_photo(m.chat.id,photo,caption=(text))
             await message.delete()
+              
+              
+              
+async def get_user_info(user, already=False):
+    if not already:
+        userss = await bot.get_chat(user)
+        user = await bot.get_users(user)
+    if not user.first_name:
+        return ["Deleted account", None]
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    mention = user.mention("Link")
+    dc_id = user.dc_id
+    is_bot = user.is_bot
+    photo_id = user.photo.big_file_id if user.photo else None
+    is_dev = user_id in dev_user
+    body = { 
+        "✪ ID": user_id,
+        "✪ DC": dc_id,
+        "✪ Name": [first_name],
+        "✪ Username": [("@" + username) if username else "Null"],
+        "✪ Mention": [mention],
+        "✪ Bot": is_bot,
+        "✪ Developer": is_dev,
+        "✪ Bio": userss.bio,
+    }
+    caption = section("User info results", body)
+    return [caption, photo_id]             
+
+
+@katsuki.on_message(filters.command("info",prefixes=HANDLSR) & filters.user(OWNER_ID))
+async def info_func(_, message):
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("Information Processing...")
+
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(str(e))
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await katsuki.download_media(photo_id)
+
+    await message.reply_photo(photo=photo, caption=info_caption, quote=False)
+    await m.delete()
+    os.remove(photo)
