5e237e11f8a2c626e2f00a021e0afef499434b5c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-03 12:22:38 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -4,10 +4,11 @@ from config import HANDLER, OWNER_ID, KATSUKI, ALIVE_TEXT
 from pyrogram import filters, __version__ as pyrover
 from Katsuki import katsuki, get_readable_time, StartTime
 
-you = await katsuki.get_me()
+
 
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def alive(_, m):
+    await you = katsuki.get_me()
     start_time = time.time()
     end_time = time.time()
     ping_time = round((end_time - start_time) * 1000, 3)
