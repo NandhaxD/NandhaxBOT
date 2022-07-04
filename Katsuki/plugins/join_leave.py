f693dff80f89ce23a774880d4cbf655e943f55a4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-04 23:22:43 +0530

    Update join_leave.py

--- Katsuki/plugins/join_leave.py
+++ Katsuki/plugins/join_leave.py
@@ -3,7 +3,13 @@ from pyrogram import filters
 from config import ( OWNER_ID, HANDLER) 
 
 @katsuki.on_message(filters.command("join",prefixes=HANDLER) & filters.user(OWNER_ID))
-async def joinchat(_, m):
+def join_chat(_, m):
           link =  m.text.split(" ")[1]
           katsuki.join_chat(link)
-          await m.reply_text(f"Successfully joined {link}")
+          m.reply_text(f"Successfully joined {link}")
+
+@katsuki.on_message(filters.command("leave",prefixes=HANDLER) & filters.user(OWNER_ID))
+def leave_chat(_, m):
+          link =  m.text.split(" ")[1]
+          katsuki.join_chat(link)
+          m.reply_text(f"Successfully joined {link}")
