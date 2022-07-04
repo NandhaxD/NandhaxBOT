d88f46d8252ef8b60a727a82196b57daf94b7575
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-04 23:27:45 +0530

    Update join_leave.py

--- Katsuki/plugins/join_leave.py
+++ Katsuki/plugins/join_leave.py
@@ -6,7 +6,10 @@ from config import ( OWNER_ID, HANDLER)
 def join_chat(_, m):
           link =  m.text.split(" ")[1]
           katsuki.join_chat(link)
-          m.reply_text(f"Successfully joined {link}")
+          except Exception as e:
+            print(e)
+          else:
+                m.reply_text(f"Successfully joined {link}")
 
 @katsuki.on_message(filters.command("leave",prefixes=HANDLER) & filters.user(OWNER_ID))
 def leave_chat(_, m):
