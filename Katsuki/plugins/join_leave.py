6dd3dc2202ff4cf68eff3ed1c75ae54173918115
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-05 09:39:15 +0530

    Update join_leave.py

--- Katsuki/plugins/join_leave.py
+++ Katsuki/plugins/join_leave.py
@@ -4,6 +4,9 @@ from config import ( OWNER_ID, HANDLER)
 
 @katsuki.on_message(filters.command("join",prefixes=HANDLER) & filters.user(OWNER_ID))
 def join_chat(_, m):
+          if len(m.command) < 2:
+               await m.reply_text("ɢɪᴠᴇ ᴀ ᴊᴏɪɴ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
+               return 
           link =  m.text.split(" ")[1]
           katsuki.join_chat(link)
           chat = katsuki.get_chat(link)
@@ -12,11 +15,12 @@ def join_chat(_, m):
 
 @katsuki.on_message(filters.command("leave",prefixes=HANDLER) & filters.user(OWNER_ID))
 def leave_chat(_, m):
+          if len(m.command) < 2:
+               await m.reply_text("ɢɪᴠᴇ ᴀ ʟᴇғᴛ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
+               return 
           link =  m.text.split(" ")[1]
           katsuki.leave_chat(link)
           chat = katsuki.get_chat(link)
           name = chat.title
           m.reply_text(f"Successfully left {name}")
 
-
-#ok I made it
