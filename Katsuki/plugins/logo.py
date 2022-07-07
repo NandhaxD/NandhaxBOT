e72127c9c56426982e5431e276d496a3565cb1b2
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-07 23:03:15 +0530

    Update logo.py

--- Katsuki/plugins/logo.py
+++ Katsuki/plugins/logo.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 
 @katsuki.on_message(filters.command("logo",prefixes=config.HANDLER) &  filters.user(OWNER_ID))
 async def logomaker(_, m):
-             if len(m.command) < 2
+             if len(m.command) < 2:
                      await m.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ʟᴏɢᴏ")
              text = m.text.split(None, 1)[1]
              msg = await m.reply_text("ʏᴏᴜʀ ʟᴏɢᴏ ᴀs ɢᴇɴᴇʀᴀᴛɪɴɢ")
