db4186479b1a6c323ea60196d6f81a4d4276cd74
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <98509058+Legend-Mukund@users.noreply.github.com>
2022-08-02 21:35:19 +0530

    ☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆

---
+++ Katsuki/plugins/block.py
@@ -0,0 +1,24 @@
+from Katsuki import katsuki 
+from pyrogram import filters 
+from config import HANDLER, OWNER_ID
+
+@katsuki.on_message(filters.command("block",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def id(_, m):
+         reply = m.reply_to_message
+         if not reply: 
+               await m.reply_text("Wʜᴏᴍ sʜᴏᴜʟᴅ ɪ ʙʟᴏᴄᴋ ᴋɪᴅᴅᴏ") 
+         if reply:
+            await reply.reply_text(f'Bʟᴏᴄᴋᴇᴅ ᴛʜɪs ɴɪɢɢᴀ {reply.from_user.first_name}')
+            await app.block_user(reply.from_user.id)
+            await m.delete()
+            
+            
+@katsuki.on_message(filters.command("block",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def id(_, m):
+         reply = m.reply_to_message
+         if not reply: 
+               await m.reply_text("Wʜᴏᴍ sʜᴏᴜʟᴅ ɪ ʙʟᴏᴄᴋ ᴋɪᴅᴅᴏ") 
+         if reply:
+            await reply.reply_text(f'Bʟᴏᴄᴋᴇᴅ ᴛʜɪs ɴɪɢɢᴀ {reply.from_user.first_name}')
+            await app.unblock_user(reply.from_user.id)
+            await m.delete()
