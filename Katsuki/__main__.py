2483e237ff486577691349222ddadc2cbe962979
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-17 20:24:19 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -1,11 +1,8 @@
-from config import LOG_GROUP_ID
-from Katsuki import katsuki, bot
-import Katsuki.plugins
+from Katsuki import katsuki
 
 
 if __name__ == "__main__":
    katsuki.start()
-   bot.run()
-   with bot:
-            bot.send_message(f"{LOG_GROUP_ID}", "Katsuki Ready Boi!")
+   with Katsuki:
+          Katsuki.send_message("me", text="Awake Now! 💀")
 
