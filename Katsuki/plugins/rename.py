d06ecbf831e23356a056d361ea1abb19c6757a9f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-18 08:44:57 +0530

    Update rename.py

--- Katsuki/plugins/rename.py
+++ Katsuki/plugins/rename.py
@@ -4,19 +4,37 @@ import os
 from pyrogram import filters
 
 
+async def FileType(message):
+    if message.documemt:
+          return message.document.mime_type.split("/")[1]
+    elif message.photo:
+          return message.photo.mime_type.split("/")[1]
+    elif message.animation:
+          return message.animation.mime_type.split("/")[1]
+    elif message.video:
+         return message.vedio.mime_type.split("/")[1]
+    else:
+         return False
+
 @katsuki.on_message(filters.command("rename",prefixes=HANDLER) & filters.user(OWNER_ID))
-def rename(_, message):
-
+async def rename(_, message):
     try:
-        filename = message.text.replace(message.text.split(" ")[0], "")
-
-    except Exception as e:
-        print(e)
-
-    if reply := message.reply_to_message:
-        x = message.reply_text("Downloading.....")
-        path = reply.download(file_name=filename)
-        x.edit("Uploading.....")
-        message.reply_document(path)
-        os.remove(path)
-
+       filename = message.text.split(None,1)[1]
+    except:
+        name = "Katsuki"
+        try:
+          if (await FileType(message=message.reply_to_message)) != False:
+               filetype = await FileType(message=message.reply_to_message)
+        except Exception as e:
+               return await message.reply_text(f"Error: `{e}`")
+        filename = "{name}.{filetype}".format(name=name, filetype=filetype)
+    msg = await message.reply_text("⬇️ File has downloading...")
+    path = await message.reply_to_message.download(file_name=filename)
+    await msg.edit_text("⬆️ File has uplaoding")
+    await message.reply_document(document=path)
+    await msg.delete()
+    os.remove(path)
+    return 
+    
+    
+    
