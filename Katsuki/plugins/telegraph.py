8627b1de8603b522da91285c6910987f232c7143
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-18 21:19:49 +0530

    Update telegraph.py

--- Katsuki/plugins/telegraph.py
+++ Katsuki/plugins/telegraph.py
@@ -6,15 +6,14 @@ from config import HANDLER,  OWNER_ID
 @katsuki.on_message(filters.command("tm", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def tm(_, message):
     await message.edit('processing...')
-    try:
-     reply_is = message.reply_to_message
-    except:
-        await message.edit("💔 Reply To The Media!")
+    reply_is = message.reply_to_message
+    if not reply_is:
+         return await message.edit_text("💔 Reply To The Media!")
     types = [True if reply_is.document else True if reply_is.photo else True if reply_is.animation else False][0]
     if types:
         path = await message.reply_to_message.download()
         grap = upload_file(path)
         for code in grap:
               url = "https://graph.org/file/"+code
-        await message.edit(str(url))
+        return await message.edit(str(url))
         
