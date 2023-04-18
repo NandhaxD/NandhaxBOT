dd73f910e7b00d5f633b4ca3d9c90176a0096163
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-18 14:14:20 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -5,14 +5,17 @@ from pyrogram import filters, __version__ as pyrover
 from Katsuki import katsuki, get_readable_time, StartTime
 
 
-
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
-def alive(_, m):
-    you = katsuki.get_me()
-    start_time = time.time()
-    end_time = time.time()
-    ping_time = round((end_time - start_time) * 1000, 3)
+async def alive(_, message):
+    name = (await katsuki.get_me()).first_name
     uptime = get_readable_time((time.time() - StartTime))
-    m.reply_animation(KATSUKI,caption=ALIVE_TEXT.format(you.mention,pyrover,ping_time,uptime))
+    alive = await message.reply_animation(KATSUKI, caption="")
+    await alive.edit_caption(f"Hello, **{name}**,\nYou Are Using Katsuki UB,\nCurrent Pyrogram Version {pyroved}, I Awake on {uptime}!")
 
-      
+katsuki.on_message(filters.command("ping",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def ping(_, message):
+     start_time = time.time()
+     end_time = time.time()
+     ping_time = round((end_time - start_time) * 1000, 3)
+     await message.edit(f"🔔 pong: {ping_time}")
+
