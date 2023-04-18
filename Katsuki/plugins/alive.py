57cc11e8602e688a277c3590b3565d8b88f806b5
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-19 00:41:37 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -8,9 +8,11 @@ from Katsuki import katsuki, get_readable_time, StartTime
 @katsuki.on_message(filters.command("alive",prefixes=HANDLER) & filters.user(OWNER_ID))
 async def alive(_, message):
     name = (await katsuki.get_me()).first_name
-    uptime = get_readable_time((time.time() - StartTime))
     alive = await message.reply_animation(KATSUKI, caption="")
-    await alive.edit_caption(f"Hello, **{name}**,\nYou Are Using Katsuki UB,\nCurrent Pyrogram Version {pyrover},\nI Awake on {uptime}!")
+    mm = ["💀","⚔️","💔","🥸","👾"]
+    for x in range(mm):
+         await alive.edit_caption(mm[x%5])
+    await alive.edit_caption(f"Hello Master, **{name}**,\nYou Are Using Katsuki And Your Current Pyrogram Version is {pyrover}! ❤️")
 
 
 @katsuki.on_message(filters.command("ping",prefixes=HANDLER) & filters.user(OWNER_ID))
@@ -18,5 +20,6 @@ async def ping(_, message):
      start_time = time.time()
      end_time = time.time()
      ping_time = round((end_time - start_time) * 1000, 3)
-     await message.edit(f"🔔 pong: {ping_time}")
+     uptime = get_readable_time((time.time() - StartTime))
+     await message.edit(f"🔔 **Pong**: {ping_time}\n⬆️ **Uptime**: {uptime}")
 
