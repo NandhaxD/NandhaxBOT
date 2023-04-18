5bf1f6c8a1faba616b35980ea05cb627b5d76868
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-18 13:48:28 +0530

    Update telegraph.py

--- Katsuki/plugins/telegraph.py
+++ Katsuki/plugins/telegraph.py
@@ -3,22 +3,18 @@ from pyrogram import filters
 from Katsuki import katsuki
 from config import HANDLER,  OWNER_ID
 
-@katsuki.on_message(filters.command("tm",prefixes=HANDLER) & filters.user(OWNER_ID))
-async def tm(_,message):
-    reply = message.reply_to_message
-    if not reply:
-          return await message.reply_text("Reply to a **Media** to get a permanent telegra.ph link.")
-    if not reply.media:
-          return await message.reply_text("Reply to a **Media** to get a permanent telegra.ph link.")
-    if reply.media:
-        msg = await message.reply_text("downloading")
-        path = await reply.download()
-        await msg.edit("uploading")
-        fk = upload_file(path)
-        for x in fk:
-           url = "https://telegra.ph" + x
-    if url.endswith("jpg"):
-             await reply.reply_photo(url,caption=f"{url}")
-    elif url.endswith("mp4"):
-             await reply.reply_animation(url,caption=f"{url}")
-    await msg.delete()
+@katsuki.on_message(filters.command("tm", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def tm(_, message):
+    m = await message.reply_text("UwU processing...")
+    try:
+     reply_is = message.reply_to_message
+    except:
+        await m.edit("💔 Reply To The Media!")
+    types = [True if reply_is.document else True if reply_is.photo else True if reply_is.animation else False][0]
+    if types:
+        path = await message.reply_to_message.download()
+        uWu = upload_file(path)
+        for code in uWu:
+              url = "https://graph.org/file/"+code
+        await m.edit(str(url))
+        
