8a377c6bfc697a85449662e874cdf6707ae9371b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-19 10:27:05 +0530

    Update admin.py

--- Katsuki/plugins/admin.py
+++ Katsuki/plugins/admin.py
@@ -39,3 +39,20 @@ async def unban_member(_, message):
     name = (await katsuki.get_users(user_id)).first_name
     return await message.edit(f"{name} Has Been UnBanned!")
 
+
+@katsuki.on_message(filters.command("purge", prefixes=config.HANDLSR) & filters.user(config.OWNER_ID))
+async def purge(_, message):
+    chat_id = message.chat.id
+    if not message.reply_to_message:
+        return await message.edit_text("Reply?")
+    else:
+        reply_msg_id = message.reply_to_message.id
+        message_id = message.id
+        message_ids = []
+        try:
+           await katsuki.delete_messages(chat_id=chat_id, message_ids=message_ids)
+        except Exception as e:
+              return await message.edit(f"Somthing wrong Happens:\n{e}")
+        return await message.edit(f"Purged {len(message_ids)} ✅")
+       
+
