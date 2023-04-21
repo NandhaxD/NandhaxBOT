2a3acc809bf445422d1d34162d9e14e1fe309f7b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-21 08:43:29 +0530

    Update dm_allow.py

--- Katsuki/plugins/dm_allow.py
+++ Katsuki/plugins/dm_allow.py
@@ -3,18 +3,17 @@ import config
 
 from pyrogram import filters, enums
 from Katsuki import katsuki
-from Katsuki.katuski_db.dm_allow import (
+from Katsuki.katusuki_db.dm_allow import (
 allowed_to_dm, disallowed_to_dm, get_allowed_users )
 
 
 
-@katsuki.on_message(group=100)
+@katsuki.on_message(filters.private)
 async def no_dm(_, message):
-    if message.chat.type == enums.ChatType.PRIVATE:
-         user_id = message.from_user.id
-         allowed_users = await get_allowed_users()
-         if not user_id == config.OWNER_ID and user_id not in allowed_users:
-              return await message.edit("UwU your not allowed dm wait for ( get approval to dm! )")
+     user_id = message.from_user.id
+     allowed_users = await get_allowed_users()
+     if not user_id == config.OWNER_ID and user_id not in allowed_users:
+          return await message.edit("UwU your not allowed dm wait for ( get approval to dm! )")
 
 
 
