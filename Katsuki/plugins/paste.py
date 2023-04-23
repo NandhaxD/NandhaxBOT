5619315cf2e182b5bad8fc855a6f162bdc83d4a7
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-23 22:06:46 +0530

    Create paste.py

---
+++ Katsuki/plugins/paste.py
@@ -0,0 +1,49 @@
+
+import config, os
+
+from Katsuki import katsuki
+from pyrogram import filters
+
+
+async def spacebin(text: str):
+    url = "https://spaceb.in/api/v1/documents/"
+    response = requests.post(url, data={"content": text, "extension": "txt"})
+    link = f"https://spaceb.in/{response.get('payload').get('id')}"
+    return link
+
+
+@katsuki.on_message(filters.command("paste",config.HANDLER) & filters.user(config.OWNER_ID))
+async def paste(_, message):
+  
+    if not message.reply_to_message:
+          try:
+              text = message.text.split(message.text.split()[0])[1]
+          except:
+               return message.edit("=> Input text to paste else reply.")
+          link = await spacebin(text)
+          return await message.edit(link)
+    elif bool(message.reply_to_message.text or message.reply_to_message.caption):
+         
+           if message.reply_to_message.text:
+                 text = message.reply_to_message.text
+           elif message.reply_to_message.caption:
+                 text = message.reply_to_message.caption
+        
+           link = await spacebin(text)
+           return await message.edit(link)
+
+    elif message.reply_to_message.document.mime_type.startswith("text/"):
+           path = await katsuki.download_media(message.reply_to_message)
+           file = open(path, "r")
+           text = file.read()
+           file.close()
+           os.remove(path)
+           link = await spacebin(text)
+           return await message.edit(link)
+    else:
+         return message.edit("=> I cannot paste this.")
+
+
+
+        
+    
