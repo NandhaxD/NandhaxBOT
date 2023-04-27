c5bfeae1392d89b592e9bd3e3419a1debf4678ff
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-27 10:10:23 +0530

    Update clone.py

--- Katsuki/plugins/clone.py
+++ Katsuki/plugins/clone.py
@@ -1,7 +1,7 @@
 
 from Katsuki import katsuki
 from pyrogram import filters
-from Katsuki.katsuki_db.clone import store_my_profile
+from Katsuki.katsuki_db.clone import store_my_profile, get_user_ids, get_my_profile
 
 import config
 
@@ -16,23 +16,20 @@ async def clone(_, message):
          clone_id = message.reply_to_message.from_user.id
 
     user_id = message.from_user.id
-    msg = await message.edit('collecting your current profile information.')
-    heh = await katsuki.get_chat(user_id)
-    bio = heh.bio
-    first_name = heh.first_name
-    profile = heh.photo.big_file_id
-
-    try:
-       await store_my_profile(
-               user_id=user_id,
-               profile=profile,
-               first_name=first_name,
-               bio=bio)
-    except Exception as e:
-           return await message.edit(f"🚫 Error in storing to db:\n{e}")
-     
     
-    msg = await message.edit('collecting cloner current profile information.')
+    if user_id not in (await get_user_ids())
+          msg = await message.edit('collecting your profile information.')
+          heh = await katsuki.get_chat(user_id)
+          bio = heh.bio
+          first_name = heh.first_name
+          profile = heh.photo.big_file_id
+
+          try:
+             await store_my_profile(user_id=user_id,profile=profile,first_name=first_name,bio=bio)
+          except Exception as e:
+                  return await message.edit(f"🚫 Error in storing to db:\n{e}")
+           
+    await msg.edit('collecting cloner profile information.')
     heh = await katsuki.get_chat(clone_id)
     bio = heh.bio
     first_name = heh.first_name
@@ -42,10 +39,34 @@ async def clone(_, message):
        await katsuki.set_profile_photo(photo=profile)
        await katsuki.update_profile(first_name=first_name, bio=bio)
     except Exception as e:
-           return await message.edit(f"🚫 Error in setting up your profile:\n{e}")
+           return await msg.edit(f"🚫 Error in setting up your profile:\n{e}")
 
-    await message.edit("✅ Successfully Implemented")
+    return await msg.edit("✅ Successfully Implemented")
     
     
     
+@katsuki.on_message(filters.command("rnclone", config.HANDLER) & filters.user(OWNER_ID))
+async def return_clone(_, message):
+      user_id = message.from_user.id
+      if user_id not in (await get_user_ids()):
+           return message.edit("You never clone's someone first clone yet!")
+      else:
+          details = await get_my_profile(user_id)
+          try:
+             bio = details['bio']
+             first_name = details['first_name']
+             photo = details['profile"]
+          except Exception as e:
+                 return await message.edit(f"Yeh somthing wrong in getting your information:\n{e}")
+         profile = await katsuki.download_media(photo)
+         try:
+            await katsuki.set_profile_photo(photo=profile)
+            await katsuki.update_profile(first_name=first_name, bio=bio)
+         except Exception as e:
+              return await message.edit(f"🚫 Error in setting up your profile:\n{e}")
+
+         return await message.edit("✅ Successfully Implemented")
     
+       
+
+
