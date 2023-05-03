3cf6e11e7dc6fad7cc45fffb7fb5e287c3cf9376
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-03 15:48:08 +0530

    Update run_code.py

--- Katsuki/plugins/run_code.py
+++ Katsuki/plugins/run_code.py
@@ -43,10 +43,10 @@ async def sh(_, message):
     if message.reply_to_message:
         reply_to_ = message.reply_to_message
 
-    if len(message.command) == 0:
+    if len(message.text.split()) == 0:
            return await message.edit("can you input the code to run program?")
 
-    code = message.text.split(message.command[0])[1]        
+    code = message.text.split(message.text.split()[0])[1]        
     x = run(code)
 
     try:
@@ -69,10 +69,10 @@ async def eval(client, message):
 
     msg = await message.edit_text("Analyzing Code...")
 
-    if len(message.command) == 0:
+    if len(message.text.split()) == 0:
            return await message.edit("can you input the code to run program?")
 
-    cmd = message.text.split(message.command[0])[1]
+    cmd = message.text.split(message.text.split()[0])[1]
     reply_to_ = message
     if message.reply_to_message:
         reply_to_ = message.reply_to_message
