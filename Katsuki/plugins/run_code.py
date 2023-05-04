207b613a114d4b91dc2f1bf60fbc052463e9d729
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-04 21:12:12 +0530

    Update run_code.py

--- Katsuki/plugins/run_code.py
+++ Katsuki/plugins/run_code.py
@@ -1,7 +1,8 @@
 import sys
 import io
-
-from subprocess import getoutput as run
+import re
+import os
+import subprocess
 import traceback
 
 from Katsuki import katsuki
@@ -35,34 +36,69 @@ async def logs(_, message):
 
 
 @katsuki.on_message(filters.user(OWNER_ID) & filters.command("sh",prefixes=HANDLER))
-async def sh(_, message):
+async def terminal(client, message):
+    if len(message.text.split()) == 1:
+        await message.edit(f"Usage: `.sh echo owo`")
+        return
+    args = message.text.split(None, 1)
+    teks = args[1]
+    if "\n" in teks:
+        code = teks.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+            try:
+                process = subprocess.Popen(
+                    shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            except Exception as err:
+                print(err)
+                await message.edit(
+                    """
+**Error:**
+```{}```
+""".format(
+                        err
+                    )
+                )
+            output += "**{}**\n".format(code)
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
+    else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", teks)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb
+            )
+            await message.edit("""**Error:**\n```{}```""".format("".join(errors)))
+            return
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            with open("output.txt", "w+") as file:
+                file.write(output)
+            await client.send_document(
+                message.chat.id,
+                "output.txt",
+                reply_to_message_id=message.id,
+                caption="`Output file`",
+            )
+            os.remove("output.txt")
+            return
+        await message.edit(f"**Output:**\n```{output}```", parse_mode="markdown")
+    else:
+        await message.edit("**Output:**\n`No Output`") 
 
-    await message.edit("Analyzing Code...")
     
-    reply_to_ = message
-    if message.reply_to_message:
-        reply_to_ = message.reply_to_message
-
-    if len(message.text.split()) == 0:
-           return await message.edit("can you input the code to run program?")
-
-    code = message.text.split(message.text.split()[0])[1]        
-    x = run(code)
-
-    try:
-
-       await reply_to_.reply_text(f"**🖥️ Code**: ```{code}```\n\n**📝 Results**:\n```{x}```")
-       return await message.delete()
-
-    except MessageTooLong:
-         with io.BytesIO(str.encode(run_logs)) as logs:
-               logs.name = "shell.txt"
-
-               await reply_to_.reply_document(
-                   document=logs, thumb=thumb_id)
-
-               return await message.delete()
-
     
 @katsuki.on_message(filters.user(OWNER_ID) & filters.command("e",prefixes=HANDLER))
 async def evaluate(client: katsuki , message):
