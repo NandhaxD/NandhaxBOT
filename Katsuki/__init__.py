94bfc3ba5c527561933a13e391ef7b3468e0d3f5
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-08 23:21:07 +0530

    Update __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -4,7 +4,7 @@ import time
 import logging
 from pyrogram import Client
 from pymongo import MongoClient 
-
+from aiohttp import ClientSession
 
 StartTime = time.time()
 
@@ -18,14 +18,19 @@ logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'
 
 
 #setup variable in hosting website environment 
-
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION") 
 
+# Pyrogram Client 
 katsuki = Client(name="Katsuki", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Katsuki"),)
 
+# pymongo database 
 DB_URL = "mongodb+srv://nandhasigma:iqKZ5OnIBVcpZde2@cluster0.gt47zau.mongodb.net/?retryWrites=true&w=majority"
 DB = MongoClient(DB_URL)
+print("=> MONGO DB CONNECTED")
 DATABASE = DB.MAIN
 
+# aiohttp session
+session = ClientSession()
+print("=> AIOHTTP SESSION CONNECTED!")
