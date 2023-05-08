bf19a1d9686720a265c64bdfcbbe3baf6b7679d9
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-08 22:48:26 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -20,9 +20,8 @@ async def alive(_, message):
 Greetings, Master {mention}! 
 I, Katsuki, am an anime-themed user bot
 who possesses an abundance of commands at your service. 
-Let me aid you in your endeavors with my unique 
-skills and abilities. Together, we shall conquer 
-the digital universe!
+Let me aid you in your endeavors with my unique skills and abilities. Together, 
+we shall conquer the digital universe!
 
 > Live Pyrogram Version is {pyrover}
 """    
