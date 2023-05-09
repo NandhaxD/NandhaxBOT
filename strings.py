e498f234aa481df1bd17b3036f649652523eccf7
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 20:03:03 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,12 @@
 
 
+PAST_FORMAT = """\n
+**Paste link**: **[Paste link]({link})**
+**Paste Raw link**: **[Raw View]({raw})**
+**Created at**: {timedate}
+"""
+
+
 BIG_NAME = """
 ┏━┓╋┏┓╋╋╋╋╋╋╋┏┳┓
 ┃┃┗┓┃┃╋╋╋╋╋╋╋┃┃┃
