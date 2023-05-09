0b2f854e9c33324cfc4d08a10e2bd36e70f68881
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 20:53:55 +0530

    Update inline.py

--- Katsuki/plugins/inline.py
+++ Katsuki/plugins/inline.py
@@ -8,25 +8,37 @@ from pyrogram import filters
 from pyrogram.types import (
 InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton )
 
+from pyrogram.raw.types import KeyboardButtonSwitchInline
+
+def error_inline_query(app, inline_query, text):
+     Inline = await app.app.answer_inline_query(
+     inline_query.id,
+     cache_time=0,
+     results = [
+        InlineQueryResultArticle(
+           "🥴 Somthing wrong Happens!",
+        InputTextMessageContent(text), disable_web_page_preview=True,
+         reply_markup=InlineKeyboardMarkup([[KeyboardButtonSwitchInline(text="TAP 👈", query=text),]]))]
+     return inline
+            
+
 @app.on_inline_query(filters.regex("paste") & filters.user(config.OWNER_ID))
 async def paste(_, inline_query):
     string = inline_query.query
     try:
-       STRING = string.split(maxsplit=2)[2]
+       STRING = string.split(maxsplit=2)[1]
     except:
-        pass
+        await error_inline_query(app=app, inline_query=inline_query, text="paste hello")
     mm = await spacebin(STRING)
     link = mm["result"]["link"]
     raw = mm["result"]["raw"]
     await app.answer_inline_query(
        inline_query.id,
        cache_time=0,
-       switch_pm_text="paste <text>",
-       switch_pm_parameter="inline",
     results=[
        InlineQueryResultArticle(
             "Here the Is The Paste!",
-            InputTextMessageContent(message_text="CLICK BELOW BUTTONS TO VIEW PASTE!", disable_web_page_preview=True),
+            InputTextMessageContent(message_text="BELOW BUTTONS TO VIEW PASTE!", disable_web_page_preview=True),
      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡️ PAST", url=link),
        InlineKeyboardButton("➡️ RAW", url=raw)]]))])
 
