81613a22bb0b4d99b86fc70a83c67b7cb68baf16
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 15:27:52 +0530

    Update assistant.py

--- Katsuki/plugins/assistant.py
+++ Katsuki/plugins/assistant.py
@@ -5,12 +5,16 @@ from Katsuki import app, katsuki_info , katsuki
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+x = []
 
 @app.on_message(filters.command("start") | filters.incoming  & filters.private)
 async def start(_, message):
      info = await katsuki_info()
      name = info.first_name
      id = info.id
+     if message.from_user.id in x:
+         return await message.reply("DON'T SPAM HERE!")
+     x.append(message.from_user.id)
      await message.forward(config.LOG_GROUP_ID)
      mention = "[{name}](tg://user?id={id})"
      BUTTON=InlineKeyboardMarkup([[
@@ -19,4 +23,6 @@ async def start(_, message):
 Hello, I am Assistant for **{mention}**
 You can deploy Your Own, To Use Me.
 """,quote=True, reply_markup=BUTTON ,parse_mode=enums.ParseMode.MARKDOWN)
-
+     await asyncio.sleep(20)
+     x.remove(message.from_user.id)
+     return 
