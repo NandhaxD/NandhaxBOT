59bbd74d95b9b2e59660af50373f5c3310fbfdf3
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 07:14:39 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -3,6 +3,7 @@ from Katsuki import katsuki
 
 if __name__ == "__main__":
    katsuki.run()
+   print("KATSUKI STARTED!")
    with katsuki:
           katsuki.send_message("me", text="Awake Now! 💀")
 
