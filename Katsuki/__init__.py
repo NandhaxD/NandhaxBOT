2e9191915b4ea77db84a83d018c160b8f0478250
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 13:07:19 +0530

    Update __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -1,6 +1,7 @@
 
 import os 
 import time
+import sys
 import logging
 from pyrogram import Client
 from pymongo import MongoClient 
@@ -16,17 +17,33 @@ logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'
 
 
 
-#setup variable in hosting website environment 
+# SETUP VARIABLE IN HOSTING WEBSITE ENVIRONMENT 
+
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("TOKEN")
 SESSION = os.getenv("SESSION") 
 
-# Pyrogram Client 
-katsuki = Client(name="Katsuki", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Katsuki"),)
+# PYROGRAM USER CLIENT 
+katsuki = Client(name="Katsuki", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Katsuki/"),)
 
-# pymongo database 
+
+#PYROGRAM BOT CLIENT
+app = Client(name="KatsukiBot", api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Katsuki/"),)
+
+# PYMONGO DATABASE
 DB_URL = "mongodb+srv://nandhasigma:iqKZ5OnIBVcpZde2@cluster0.gt47zau.mongodb.net/?retryWrites=true&w=majority"
 DB = MongoClient(DB_URL)
-print("=> MONGO DB CONNECTED")
+
+try:
+   DB..server_info()
+except ConnectionFailure:
+     logging.info("Connection failure, Invalid MONGOB URL!, DOWN!")
+     sys.exit()
+
+
 DATABASE = DB.MAIN
+
+
+
 
