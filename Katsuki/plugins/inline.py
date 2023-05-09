0d723ed48b673814db7ab876ce5627a91f1baec8
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 14:22:41 +0530

    Update inline.py

--- Katsuki/plugins/inline.py
+++ Katsuki/plugins/inline.py
@@ -7,9 +7,10 @@ from pyrogram.types import (
 InlineQueryResultArticle, InputTextMessageContent )
 
 @app.on_inline_query(filters.regex("hello") & filters.user(config.OWNER_ID))
-async def hello(_, query):
+async def hello(_, inline_query):
     await app.answer_inline_query(
-       query.id,
+       inline_query.id,
+       cache_time=0,
     results=[
        InlineQueryResultArticle(
             "Here the Is The Help!",
