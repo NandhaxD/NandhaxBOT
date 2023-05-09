739a66c870923d843ce21916e818eecf3b6f1178
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 19:46:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 LOG_GROUP_ID = -1001911831321
-OWNER_ID = [6129152989]
+OWNER_ID = 6129152989
 HANDLER = ["~", ".","!","?","@","$"]
 KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
-
+SOURCE = "https://gitHub.com/nandhaxd/katsuki"
