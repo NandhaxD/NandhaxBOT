b00f6a2d4a7aab1167a1cbf733f6cfde75e70ce0
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 15:39:04 +0530

    Update inline.py

--- Katsuki/plugins/inline.py
+++ Katsuki/plugins/inline.py
@@ -1,18 +1,23 @@
+
 import config 
 
 from Katsuki import app
+from Katsuki.helpers.help_func import spacebin
 from pyrogram import filters
 
 from pyrogram.types import (
 InlineQueryResultArticle, InputTextMessageContent )
 
-@app.on_inline_query(filters.regex("hello") & filters.user(config.OWNER_ID))
+@app.on_inline_query(filters.regex("paste") & filters.user(config.OWNER_ID))
 async def hello(_, inline_query):
+    string = inline_query.query
+    STRING = string.split(maxsplit=1)[1]
+    MMMM = spacebin(STRING)
     await app.answer_inline_query(
        inline_query.id,
        cache_time=0,
     results=[
        InlineQueryResultArticle(
-            "Here the Is The Help!",
-            InputTextMessageContent('hello master 😘'))])
+            "Here the Is The Paste!",
+            InputTextMessageContent(message_text=MMMM, disable_web_page_preview=True))])
 
