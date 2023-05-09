5e8eee7bd1ce7e6c7419df17a32aa7b43a8cd8f4
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 21:22:39 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,18 @@
 
 
+ALIVE_TEXT = """\n
+     {name}
+
+Greetings, Master {mention}! 
+I, Katsuki, am an anime-themed user bot
+who possesses an abundance of commands at
+your service. Let me aid you in your endeavors 
+with my unique skills and abilities. Together, 
+we shall conquer the digital universe!
+
+> Live Pyrogram Version is {pyrover}
+"""    
+
 PAST_FORMAT = """\n
 **Paste link**: **[Paste link]({link})**
 **Paste Raw link**: **[Raw View]({raw})**
