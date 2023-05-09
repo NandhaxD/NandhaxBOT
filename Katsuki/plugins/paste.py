4200bcc1fbd4c76b2fb50397520742b541035e78
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 15:29:40 +0530

    Update paste.py

--- Katsuki/plugins/paste.py
+++ Katsuki/plugins/paste.py
@@ -7,32 +7,9 @@ from pyrogram import filters, enums
 
 
 
-async def convert_to_datetime(timestamp): # Unix timestamp
-     date = datetime.datetime.fromtimestamp(timestamp)
-     return date
-
-
-async def spacebin(text: str):
-    url = "https://spaceb.in/api/v1/documents/"
-    response = requests.post(url, data={"content": text, "extension": "txt"})
-    id = response.json().get('payload').get('id')
-    res = requests.get(f"https://spaceb.in/api/v1/documents/{id}").json()
-    created_at = res.get("payload").get("created_at")
-    link = f"https://spaceb.in/{id}"
-    raw = f"https://spaceb.in/api/v1/documents/{id}/raw"
-    timedate = await convert_to_datetime(created_at)
-    string = f"""\n
-**Paste link**: **[Paste link]({link})**
-**Paste Raw link**: **[Raw View]({raw})**
-**Created at**: {timedate}
-"""
-    return string
 
     
     
-    
-
-
 
 @katsuki.on_message(filters.command("paste",config.HANDLER) & filters.user(config.OWNER_ID))
 async def paste(_, message):
