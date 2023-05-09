c4f304c9828eff0510b942a36ddeb080cd7388bd
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 14:59:03 +0530

    Update assistant.py

--- Katsuki/plugins/assistant.py
+++ Katsuki/plugins/assistant.py
@@ -1,13 +1,18 @@
 from pyrogram import filters, enums
-from Katsuki import app, katsuki, mention 
+from Katsuki import app, katsuki_info , mention 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+"""
 @app.on_message(filters.command("start") & filters.private)
 async def start(_, message):
-     MENTION = await mention()
+     info = await katsuki_info()
+     name = info.first_name
+     id = info.id
+     mention = "[{name}](tg://user?id={id})"
      BUTTON=InlineKeyboardMarkup([[
      InlineKeyboardButton("Source 👾", url="https://github.com/nandhaxd/Katsuki"),]])
      return await message.reply_text(f"""\n
-Hello, I am Assistant for **{MENTION}**
+Hello, I am Assistant for **{mention}**
 You can deploy Your Own, To Use Me.""",quote=True, reply_markup=BUTTON ,parse_mode=enums.ParseMode.MARKDOWN)
+"""
