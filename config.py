0239880e6fca97393e3994bf872efa7b17480e45
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-09 15:20:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-LOG_GROUP_ID = -635484650
+LOG_GROUP_ID = -1001911831321
 OWNER_ID = [6129152989]
 HANDLER = ["~", ".","!","?","@","$"]
 KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
