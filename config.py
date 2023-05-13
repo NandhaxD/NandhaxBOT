47902b9607316c5d4111da66f1ea0bf6b4da30cf
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-13 22:37:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,37 @@
 
-LOG_GROUP_ID = -1001911831321
-OWNER_ID = 6129152989
-HANDLER = ["~", ".","!","?","@","$"]
-KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
+
+import os, sys
+
+
+""" CHANGE TO FALSE IF YOU DON'T WANNA ADD VARIABLES IN HOSTING SITE """
+ENV = os.getenv("ENV" True)
+
+
+if ENV:    
+   API_ID = os.getenv("API_ID")
+   API_HASH = os.getenv("API_HASH")
+   TOKEN = os.getenv("TOKEN")
+   SESSION = os.getenv("SESSION")
+   OWNER_ID = os.getenv("OWNER_ID")
+   GROUP_ID = os.getenv("GROUP_ID")
+   KATSUKI = os.getenv("KATSUKI")
+   """ YOU VALUE MOST LIKE THIS '.,!,?,' """
+   HANDLER = os.getenv("HANDLER").split(",")
+else:
+   API_ID = 1234567
+   API_HASH = "<hash>"
+   TOKEN = "<bot-token>"
+   SESSION = "<string-session>"
+   OWNER_ID = 5696053228
+   GROUP_ID = -1001717881477
+   KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
+   HANDLER = ["~", ".","!","?","@","$"]
+
+
+
+
+
 SOURCE = "https://gitHub.com/nandhaxd/katsuki"
+
+
+
