76f33c18c2e7e23b851b49437db5018a1cc9c9ab
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 00:48:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,11 @@ if ENV:
    API_HASH = os.getenv("API_HASH")
    BOT_TOKEN = os.getenv("BOT_TOKEN")
    SESSION = os.getenv("SESSION")
-   OWNER_ID = os.getenv("OWNER_ID")
-   GROUP_ID = os.getenv("GROUP_ID")
-   KATSUKI = os.getenv("KATSUKI")
+   GROUP_ID = os.getenv("GROUP_ID", -1001717881477)
+   KATSUKI = os.getenv("KATSUKI", "https://graph.org/file/56bb59a1057c3021ae8cd.mp4")
    DB_URL = os.getenv("DB_URL")
    HANDLER = os.getenv("HANDLER", "['.','!']")
-   LIST_OF_VARIABLE = ["API_ID", "API_HASH", "BOT_TOKEN", "SESSION", "OWNER_ID", "GROUP_ID", "KATSUKI", "HANDLER", "DB_URL"]   
+   LIST_OF_VARIABLE = ["API_ID", "API_HASH", "BOT_TOKEN", "SESSION", "GROUP_ID", "KATSUKI", "HANDLER", "DB_URL"]   
    for var in LIST_OF_VARIABLE:
       if os.getenv(var) == None:
            print(f"The {var} environment variable is missing.")
@@ -27,7 +26,6 @@ else:
    API_HASH = "<hash>"
    BOT_TOKEN = "<bot-token>"
    SESSION = "<string-session>"
-   OWNER_ID = 5696053228
    GROUP_ID = -1001717881477
    KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
    HANDLER = ["~", ".","!","?","@","$"]
