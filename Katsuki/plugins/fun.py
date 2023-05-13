abb33fa5c25b46ea4fdd12e4c09af6d7caff88a6
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-13 21:20:59 +0530

    Update fun.py

--- Katsuki/plugins/fun.py
+++ Katsuki/plugins/fun.py
@@ -1,1 +1,28 @@
+""" SOME FUN MODULES FOR ENTERTAINMENT """
 
+
+import config, asyncio 
+
+from Katsuki katsuki
+from pyrogram import filters
+
+
+"""ANIMATION HACKS """
+@app.on_message(filters.command("hack",config.HANDLER) & filters.user(config.OWNER_ID))
+async def hack(_, message):
+    
+    if message.reply_to_message:
+         user_id = message.reply_to_message.from_user.id
+    else:
+      return await message.edit("who should I hack ?")
+    HACK_STRING = [
+    "Looking for WhatsApp databases in targeted person...",
+    " User online: True\nTelegram access: True\nRead Storage: True ",
+    "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]",
+    "Hacking... 86.21%\n[███████████████░░░░░]",
+    "Hacking... 93.50%\n[█████████████████░░░]",
+    "hacking....  100%\n[████████████████████]",
+]
+    for string in HACK_STRING:
+          await asyncio.sleep(4)
+          await message.edit(HACK_STRING[string % 6])
