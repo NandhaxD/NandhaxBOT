376def20ab7d08c9220dc4f9b6c716c2d74a75fa
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 00:27:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ ENV = os.getenv("ENV", True)
 if ENV:    
    API_ID = os.getenv("API_ID")
    API_HASH = os.getenv("API_HASH")
-   TOKEN = os.getenv("TOKEN")
+   BOT_TOKEN = os.getenv("BOT_TOKEN")
    SESSION = os.getenv("SESSION")
    OWNER_ID = os.getenv("OWNER_ID")
    GROUP_ID = os.getenv("GROUP_ID")
    KATSUKI = os.getenv("KATSUKI")
    DB_URL = os.getenv("DB_URL")
    HANDLER = str(os.getenv("HANDLER")).split(",")
-   LIST_OF_VARIABLE = ["API_ID", "API_HASH", "TOKEN", "SESSION", "OWNER_ID", "GROUP_ID", "KATSUKI", "HANDLER", "DB_URL"]   
+   LIST_OF_VARIABLE = ["API_ID", "API_HASH", "BOT_TOKEN", "SESSION", "OWNER_ID", "GROUP_ID", "KATSUKI", "HANDLER", "DB_URL"]   
    for var in LIST_OF_VARIABLE:
       try:
           locals()[var] = os.getenv(var)
@@ -27,7 +27,7 @@ if ENV:
 else:
    API_ID = 1234567
    API_HASH = "<hash>"
-   TOKEN = "<bot-token>"
+   BOT_TOKEN = "<bot-token>"
    SESSION = "<string-session>"
    OWNER_ID = 5696053228
    GROUP_ID = -1001717881477
