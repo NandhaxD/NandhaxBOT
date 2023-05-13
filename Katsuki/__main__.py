8f6e5d9877f39df49e525bc762cc25b1728b2c24
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-13 23:56:06 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -2,15 +2,16 @@ import pyrogram
 import strings
  
 from Katsuki import bot , app
-from Katsuki.helpers.help_func import datetime 
+from Katsuki.helpers.help_func import get_datetime 
 
 async def run_clients():
       await bot.start()
       await app.start()
       await pyrogram.idle()
+      zone = await get_datetime()
       await bot.send_message(
            chat_id=config.GROUP_ID,
-           text=strings.RESTART_TEXT)
+           text=strings.RESTART_TEXT.format(zone["date"], zone["time"])
       
 
 
