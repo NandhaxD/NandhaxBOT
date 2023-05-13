42f78a28df18375ef7bff967a2c33d3e34f3bc1c
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 00:22:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ if ENV:
    GROUP_ID = os.getenv("GROUP_ID")
    KATSUKI = os.getenv("KATSUKI")
    DB_URL = os.getenv("DB_URL")
-   HANDLER = os.getenv("HANDLER").split(",")
+   HANDLER = str(os.getenv("HANDLER")).split(",")
    LIST_OF_VARIABLE = ["API_ID", "API_HASH", "TOKEN", "SESSION", "OWNER_ID", "GROUP_ID", "KATSUKI", "HANDLER", "DB_URL"]   
    for var in LIST_OF_VARIABLE:
       try:
@@ -33,7 +33,7 @@ else:
    GROUP_ID = -1001717881477
    KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
    HANDLER = ["~", ".","!","?","@","$"]
-
+   DB_URL = "<mongodb-url>"
 
 
 
