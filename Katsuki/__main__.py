ddb31e08b81cac361ee015682abca48ac6883020
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-13 23:57:41 +0530

    Update __main__.py

--- Katsuki/__main__.py
+++ Katsuki/__main__.py
@@ -11,7 +11,8 @@ async def run_clients():
       zone = await get_datetime()
       await bot.send_message(
            chat_id=config.GROUP_ID,
-           text=strings.RESTART_TEXT.format(zone["date"], zone["time"])
+           text=strings.RESTART_TEXT.format(
+   date=zone["date"], time=zone["time"])
       
 
 
