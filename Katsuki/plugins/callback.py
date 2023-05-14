c600987b40b3436b324fe21456f615977b485f46
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-15 00:26:34 +0530

    Update callback.py

--- Katsuki/plugins/callback.py
+++ Katsuki/plugins/callback.py
@@ -13,7 +13,7 @@ async def help_back(_, query):
    user_id = (await GET_INFO.app()).id
    if not query.from_user.id == int(user_id):
        return await query.answer("😤 You aren't my master")
-   buttons = [[InlineKeyboardButton(x['module'], callback_data=f"help:{x.get('module')}")] for x in MODULE]
+   buttons = [[InlineKeyboardButton(x['module'], callback_data=f"help:{x['module']}")] for x in MODULE]
    return await bot.edit_inline_text(
        inline_message_id=query.inline_message_id, text="HELP COMMANDS!", reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.MARKDOWN)
        
