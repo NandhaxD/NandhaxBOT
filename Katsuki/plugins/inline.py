7c6b44567d23ea19b4b48446b804bea912c9d072
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 22:54:55 +0530

    Update inline.py

--- Katsuki/plugins/inline.py
+++ Katsuki/plugins/inline.py
@@ -24,31 +24,21 @@ async def help_cmds(_, inline_query):
       cache_time=0,
       results = [
      InlineQueryResultArticle(
-        "🆘 Help Commands",  InputTextMessageContent(message_text="Help Commands!"), thumb_url="https://graph.org/file/b136511bda43b1d8db7d2.jpg",reply_markup=InlineKeyboardMarkup(buttons))])
+        "🆘 HELP COMMANDS",  InputTextMessageContent(message_text="Help Commands!"), thumb_url="https://graph.org/file/b136511bda43b1d8db7d2.jpg",reply_markup=InlineKeyboardMarkup(buttons))])
 
 
-@bot.on_inline_query(filters.regex("paste"))
-async def paste(_, inline_query):
+@bot.on_inline_query(filters.regex("test"))
+async def test(_, inline_query):
     user_id = (await GET_INFO.app()).id
     if not inline_query.from_user.id == user_id:
        return 
-    string = inline_query.query
-    try:
-       CONTENT = string.split(maxsplit=1)[1]
-    except:
-       return 
-    mm = await spacebin(CONTENT)
-    link = mm["result"]["link"]
-    raw = mm["result"]["raw"]
+    string = inline_query
     await bot.answer_inline_query(
        inline_query.id,
        cache_time=0,
     results=[
        InlineQueryResultArticle(
-            "Paste Success ✅",
-            InputTextMessageContent(message_text="BELOW BUTTONS TO VIEW PASTE!", disable_web_page_preview=True), thumb_url="https://graph.org/file/4f71af878a085505e8faf.jpg",
-     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("➡️ PASTE", url=link),
-       InlineKeyboardButton("➡️ RAW", url=raw)]]))])
-
-
+            "Here the InlineQuery Objecs",
+            InputTextMessageContent(message_text=string, disable_web_page_preview=True), thumb_url="https://graph.org/file/4f71af878a085505e8faf.jpg")])
+     
 
