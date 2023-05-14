6bc378d0220000f683fa60e0b955f4530717c83b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 14:03:56 +0530

    Update callback.py

--- Katsuki/plugins/callback.py
+++ Katsuki/plugins/callback.py
@@ -1,1 +1,23 @@
+from Katsuki import bot, MODULE, INFO as GET_INFO
+from pyrogram.types import (
+InlineKeyboardButton InlineKeyboardMarkup)
 
+
+
+@bot.on_callback_queey(filters.regex('^help'))
+async def help_commnds(_, query):
+   user_id = (await GET_INFO()).id
+   if not query.from_user.id == int(user_id):
+       return await query.answer('😤 You aren't my master')
+   CB_NAME = query.data.split(':')[1].casefold()
+   data = [x for x in MODULE if x['module'].casefold() == CB_NAME]
+   if len(data) == 0:
+       return await query.answer("🤔 somthing wrong.")
+   module = data[0]['module']
+   help = data[0]['help']
+   return await query.message.edit(strings.HELP_CMD)
+       else:
+            
+            
+
+   
