ec655850dac7df8c59d69957b41d9bc1711bc31f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 23:52:22 +0530

    Update __init__.py

--- Katsuki/__init__.py
+++ Katsuki/__init__.py
@@ -12,19 +12,29 @@ StartTime = time.time()
 
 MODULE = []
 
-# LOGGING INFO
-FORMAT = "[Katsuki Client]: %(message)s"
-logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
-              logging.StreamHandler()], format=FORMAT)
+
+
+class CustomFilter(logging.Filter):
+    def filter(self, record):
+        if "Loaded Plugins from" in record.msg:  # replace the message
+            record.msg = record.msg.replace("Loaded Plugins from", "Plugins loaded from")
+        return True
+
+logging.getLogger("pyrogram").setLevel(logging.DEBUG)  # set logging level
+handler = logging.FileHandler("logs.txt")  # create file handler
+handler.setFormatter(logging.Formatter("%(asctime)s [%(name)s]: %(message)s"))  # use default formatter
+handler.addFilter(CustomFilter())  # add custom filter to handler
 
 
 
 # PYROGRAM USER CLIENT 
-app = Client(name="katsuki", session_string=config.SESSION, api_id=config.API_ID, api_hash=config.API_HASH, plugins=dict(root="Katsuki"),)
+app = Client(name="katsuki", session_string=config.SESSION, api_id=config.API_ID, api_hash=config.API_HASH, plugins=dict(root="Katsuki"),
+logging_level=logging.DEBUG, handlers=[handler])
 
 
 #PYROGRAM BOT CLIENT
-bot = Client(name="KatsukiBot", bot_token=config.BOT_TOKEN, api_id=config.API_ID, api_hash=config.API_HASH, plugins=dict(root="Katsuki"),)
+bot = Client(name="KatsukiBot", bot_token=config.BOT_TOKEN, api_id=config.API_ID, api_hash=config.API_HASH, plugins=dict(root="Katsuki"),
+logging_level=logging.DEBUG, handlers=[handler])
 
 # PYMONGO DATABASE
 DB = MongoClient(config.DB_URL)
