7269b9ced657e90f0a3389a318bcc75f430e55fb
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 23:27:03 +0530

    Update telegraph.py

--- Katsuki/plugins/telegraph.py
+++ Katsuki/plugins/telegraph.py
@@ -1,7 +1,13 @@
-from telegraph import upload_file
+from telegraph import upload_file, Telegraph
 from pyrogram import filters
 from Katsuki import app
 import config
+
+
+
+
+
+""" TELEGRAPH MEDIA UPLOADER """
 
 @app.on_message(filters.command("tm", prefixes=config.HANDLER) & filters.me)
 async def tm(_, message):
@@ -16,4 +22,26 @@ async def tm(_, message):
         for code in grap:
               url = "https://graph.org"+code
         return await message.edit(str(url))
-        
+
+
+
+""" TELEGRAPH PAGE CREATOR """
+
+telegraph = Telegraph()
+telegraph.create_account(short_name=config.NAME)
+
+
+@app.on_message(filters.command("txt", prefixes=config.HANDLER))
+async def graph_text(_, message):
+     
+     first_name=message.from_user.first_name
+     if len(message.text.split()) <= 2:
+           text = message.text.split(maxsplit=1)[1]
+     else:
+          if (message.reply_to_message and message.reply_to_message.text or message.reply_to_message.caption):
+                text = (message.reply_to_message.text or message.reply_to_message.caption)
+          else:
+              return await message.edit('reply to the text for give some text to upload telegraph')
+          response = telegraph.create_page(first_name, html_content=text)
+          page_url = response['url']
+          return await message.edit(page_url)
