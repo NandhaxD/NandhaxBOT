54666ba9cd529a4de6313d6f46d047a9f4c03b4a
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-14 09:55:04 +0530

    Update misc.py

--- Katsuki/plugins/misc.py
+++ Katsuki/plugins/misc.py
@@ -7,6 +7,34 @@ from gpytranslate import Translator
 
 import requests
 
+@app.on_message(filters.me & filters.command("git",prefixes=config.HANDLER)) 
+ async def git(_, message): 
+     if len(message.command) < 2: 
+         return await message.reply_text("where you input the username?\n") 
+     user = message.text.split(None, 1)[1] 
+     res = requests.get(f'https://api.github.com/users/{user}').json() 
+     data = f"""**Name**: {res['name']} 
+ **UserName**: {res['login']} 
+ **Link**: [{res['login']}]({res['html_url']}) 
+ **Bio**: {res['bio']} 
+ **Company**: {res['company']} 
+ **Blog**: {res['blog']} 
+ **Location**: {res['location']} 
+ **Public Repos**: {res['public_repos']} 
+ **Followers**: {res['followers']} 
+ **Following**: {res['following']} 
+ **Acc Created**: {res['created_at']} 
+ """ 
+     with open(f"{user}.jpg", "wb") as f: 
+         kek = get(res['avatar_url']).content 
+         f.write(kek) 
+  
+     await message.reply_photo(f"{user}.jpg", caption=data, quote=True) 
+     os.remove(f"{user}.jpg") 
+     await message.delete() 
+     return 
+
+
 
 
 @app.on_message(filters.command(["ud","define"],prefixes=config.HANDLER) & filters.me)
@@ -54,3 +82,21 @@ async def translate(_, message) -> None:
     await message.delete()
     await reply_msg.reply_text(reply)
     return 
+
+
+
+
+__mod_name__ = "MISC"  
+    
+  __help__ = """\n  
+ - git: find github user 
+ - tr {code}: reply to the message 
+ - ud: urban dictionary 
+  
+  """  
+    
+    
+    
+    
+  string = {"module": __mod_name__, "help": __help__}   
+  MODULE.append(string)
