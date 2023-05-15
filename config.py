c97cb9fcfe0f2cdfef2047a0fa1d60745d5dd3c6
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-16 00:11:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,24 +15,31 @@ if ENV:
    SESSION = os.getenv("SESSION")
    GROUP_ID = os.getenv("GROUP_ID")
    DB_URL = os.getenv("DB_URL")
-   HANDLER = ["~", ".","!","?","@","$"]
+   HANDLER = os.getenv("HANDLER")
    LIST_OF_VARIABLE = ["API_ID", "API_HASH", "BOT_TOKEN", "SESSION", "GROUP_ID", "DB_URL"]   
    for var in LIST_OF_VARIABLE:
       if not os.getenv(var):
            sys.exit()
            print(f"THE {var} ENVIRONMENT VARIABLE IS MISSING.")
 else:
-   API_ID = 1234567
-   API_HASH = "<hash>"
-   BOT_TOKEN = "<bot-token>"
-   SESSION = "<string-session>"
-   GROUP_ID = -1001717881477
-   KATSUKI = "https://graph.org/file/56bb59a1057c3021ae8cd.mp4"
-   HANDLER = ["~", ".","!","?","@","$"]
-   DB_URL = "<mongodb-url>"
+   API_ID = None #int() method | get from my.telegram.org
+   API_HASH = None #str() method | get from my.telegram.org
+   BOT_TOKEN = None #str() method | get via @botfather
+   SESSION = None #str() method | use replit or bots to get your Pyro session 
+   GROUP_ID = None #int() menthod | your group id
+   KATSUKI = None # your alive photo
+   HANDLER = None # example ".,?".split(",")
+   DB_URL = None # get from mongodb.com
+
+
+""" DEFAULT VARIABLES """
+
+if not HANDLER:
+     HANDLER = ["~", ".","!","?","@","$"]
 
 
 
+""" REQUIRED VARIABLES """
 
 SOURCE = "https://github.com/nandhaxd/katsuki"
 NAME = "Katsuki"
