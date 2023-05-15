f1d0ba4f639c79afb84fd690d22cf8eb577bf758
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-15 23:32:36 +0530

    Update admin.py

---
+++ Katsuki/plugins/admin.py
@@ -0,0 +1,27 @@
+import config 
+
+
+from pyrogram import filters
+from Katsuki import app
+from Katsuki.helpers.decorators import admin_only
+
+
+
+@admin_only
+@app.on_message(filters.command("ban", config.HANDLER) & filters.me)
+async def ban(_, message):
+   
+   if len(message.text.split()) >= 2:
+        user_id = message.from_user.id
+   elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+   else:
+       return await message.edit("WHO SHOULD I BAN?")
+   try:
+      IS_BAN = await app.ban_chat_member(
+           chat_id=message.chat.id,
+           user_id=user_id)
+   except Exception as e:
+         return await message.edit("[`ERROR`: `{e}`]")
+   
+  
