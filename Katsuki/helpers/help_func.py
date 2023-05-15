d8a6a31736cfba0a7a37fd84eaf38a49b5301adb
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-16 00:25:15 +0530

    Update help_func.py

--- Katsuki/helpers/help_func.py
+++ Katsuki/helpers/help_func.py
@@ -1,5 +1,8 @@
 
 
+"""
+Copyright © [2023-2024] @NandhaBots. All rights reserved. Reproduction, modification, distribution, or republication of this file without prior written permission from @NandhaBots is strictly prohibited. The Katsuki Telegram user bot has been developed with the Pyrogram library and utilizing Python programming language, making it a safe and secure option for users. Unauthorized use of this bot or any part of it may result in legal action. This project is owned by @Nandha, and any unauthorized use or distribution of this bot is strictly prohibited.
+"""
 
 
 
