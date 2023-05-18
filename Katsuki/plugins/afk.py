0fe2b90a1c8395ef9204bfac8bc6e57e65f48f7e
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-18 10:36:03 +0530

    Create afk.py

---
+++ Katsuki/plugins/afk.py
@@ -0,0 +1,12 @@
+
+
+import config, strings
+
+from Katsuki import app
+
+from Katsuki.database import afk import (
+
+add_afk, remove_afk, is_afk, get_afk_reason)
+
+from pyrogram import filters, enums
+
