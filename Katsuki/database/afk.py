a835a67566db5bc62b5a91a2400f5a0a28962aff
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-18 10:25:24 +0530

    Update afk.py

--- Katsuki/database/afk.py
+++ Katsuki/database/afk.py
@@ -26,16 +26,16 @@ async def update_delete(user_id: int, is_delete: bool):
      if db.file_one(find):
      	update = {"$set": {"is_delete": is_delete}}
      	db.update_one(filter=find, update=update)
-         return True
+      return True
      return False
      
 
 async def is_afk(user_id: int):
 	  string = {"user_id": user_id}
  	 is_afk=db.find_one(string)
- 	 if is_afk:
- 		 return True
-      return False
+   if is_afk:
+ 		   return True
+   return False
 
 async def get_afk_reason(user_id: int):
 	  string = {"user_id": user_id}
