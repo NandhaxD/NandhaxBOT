1661d92ccd6a7ecfe1e6f18416f6fd6bcf6d3ee7
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-25 11:13:52 +0530

    Update afk.py

--- Katsuki/plugins/afk.py
+++ Katsuki/plugins/afk.py
@@ -20,11 +20,11 @@ async def afk_turn_on(_, message):
               reason = message.text.split(None,1)[1] 
           else: 
               reason = "Busy 🦥"
+          await message.edit('[`YOU TURN ON AFK NOW`]')
           date = (await get_datetime())['date'] 
           time = (await get_datetime())['time']           
           data = {'AFK': True, 'date': date,'time': time, 'reason': reason} 
           DATA.update(data) 
-          return await message.edit('[`YOU TURN ON AFK NOW`]')
                                             
      
 
