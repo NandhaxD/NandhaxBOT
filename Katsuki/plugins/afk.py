5b3db9d88e87ecafb68434b9743189b4a572315f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-25 09:27:55 +0530

    Update afk.py

--- Katsuki/plugins/afk.py
+++ Katsuki/plugins/afk.py
@@ -36,10 +36,11 @@ async def afk_turn_on(_, message):
 
           
 
+
 @app.on_message((filters.group | filters.private) & filters.me) 
  async def afk_turn_off(_, message): 
   
-     try: 
+    try: 
           if DATA.get('AFK'): 
                     DATA.clear() 
                     return await message.reply_text("[`WELCOME BACK MASTER` 👾]") 
