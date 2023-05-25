5f23cfa4e86546f9f08804ffa6083bc96307db87
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-25 08:15:28 +0530

    Update afk.py

--- Katsuki/plugins/afk.py
+++ Katsuki/plugins/afk.py
@@ -1,1 +1,70 @@
 
+
+
+
+import config
+from pyrogram import filters
+from Katsuki.helpers.help_func import datetime 
+from Katsuki import app
+
+
+
+DATA = {}
+
+
+
+
+
+
+@app.on_message(filters.me & filters.command("afk", prefixes=config.HANDLER))
+async def afk_turn_on(_, message):
+	 date = datetime['date']
+	 time = datetime['time']
+	 if len(message.text.split()) >= 2:
+	 	  reason = message.text.split(None,1)[1]
+	 else:
+	 	  reason = "Busy 🦥"	
+	 data = {'AFK': True, 'date': date, 'time': time, 'reason': reason}
+     DATA.update(data)
+     return await message.edit('[`YOU TURN ON AFK NOW`]')
+     
+     
+
+          
+@app.on_message((filters.group | filters.private) & filters.me)
+async def afk_turn_off(_, message):
+	  try:
+	     if DATA.get('AFK'):
+	    	  DATA.clear()
+	          return await message.reply_text("[`WELCOME BACK MASTER` 👾]")
+	     else:
+	        return 
+	  except:
+	       pass
+	      
+	  	
+	 
+	 
+@app.on_message(filters.group & filters.reply ~filters.me)
+async def telling_is_afk(_, message):
+	   
+	   katsuki_id = int(10000000)
+	   try:
+	      if not DATA.get('AFK'):
+	          return
+	      elif message.reply_to_message.from_user.id == katsuki_id:
+	           date = DATA['date']
+	           time = DATA['time']
+	           reason = DATA['reason']
+	           return await message.reply_text(strings.AFK_STRING.format(
+	           reason=reason, date=date, time=time))
+   	except:
+   	     pass	             	              
+	             
+	         
+      
+	 
+	 	
+	 
+	 
+	 
