4e47dc91c11074baa04d7b890ca9193c80cd4964
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-26 15:36:11 +0530

    Update misc.py

--- Katsuki/plugins/misc.py
+++ Katsuki/plugins/misc.py
@@ -25,21 +25,20 @@ AI = False
 
 @app.on_message(filters.me & filters.command("ai", config.HANDLER))
 async def chatbot_turn_on(_, message ):
-	 global AI 
-	 pertten = ['on', 'off']
-	 if not len(message.text.split()) >= 2:
-	 	 return await message.edit('pertten: on|off')
+	 global AI
+  pertten = ['on', 'off']
+  if not len(message.text.split()) >= 2:
+       return await message.edit('pertten: on|off')
   query = message.text.split()[1]
   if not query in pertten:
-     	  return await message.edit('pertten: on|off')
+        return await message.edit('pertten: on|off')
   else:
-     
-       if query == 'on':
-             AI = True
-             return await message.edit('`AI MOD TRUN ON`')              
-       elif query == 'off':
-                   AI = False
-                   return await message.edit('`AI MOD TRUN OFF`')
+      if query == 'on':
+            AI = True
+            return await message.edit('`AI MOD TRUN ON`')
+      elif query == 'off':
+             AI = False
+             return await message.edit('`AI MOD TRUN OFF`')
 
          
      		  
