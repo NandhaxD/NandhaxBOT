2ef2aefc802a39b6ef54fe83f3a70467fb26c7f5
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-26 08:01:18 +0530

    Update admin.py

--- Katsuki/plugins/admin.py
+++ Katsuki/plugins/admin.py
@@ -6,12 +6,54 @@ Copyright © [2023-2024] @NandhaBots. All rights reserved. Reproduction, modific
 
 
 
-import config 
+import config, asyncio 
 
 
 from pyrogram import filters
-from Katsuki import app
+from Katsuki import app, bot
 from Katsuki.helpers.decorator import admin_only, can_restrict_members
+
+
+
+@app.on_message(filters.me & filters.command("banall", config.HANDLER))
+@can_restrict_members
+async def ban_all_members(_, message):
+	
+	  chat_id = message.chat.id
+	  
+	  success = 0
+	  failures = 0
+	  
+	  string = f"""
+#MASS_BAN_EVENT
+
+From: [ `{chat_id}` ]
+	  	  
+Successfully Banned: [ `{success}` ]
+Failure to Banned: [ `{failures}` ]
+""" 	  
+	
+	  async for m in app.get_chat_members(chat_id=chat_id):
+	  	    
+	  	   try:
+	  	   	 
+	  	      cmd = await app.ban_chat_member(
+	  	         chat_id=chat_id, user_id=user_id)
+	  	         
+	  	      if cmd:
+	  	      	success+= 1
+	  	      	
+	  	      await asyncio.sleep(3)
+	  	      await message.edit(string)
+	  	      
+	  	   except:
+	  	   	  failures += 1
+	   
+	  await bot.send_message(
+	       chat_id=config.GROUP_ID,
+	       text=string)
+   string += "\nStatus: [`DONE`]"
+	  	   	
 
 
 
