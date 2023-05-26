0f84686c850e8c718c2c0028c7abe728b96c63e2
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-26 15:23:24 +0530

    Update misc.py

--- Katsuki/plugins/misc.py
+++ Katsuki/plugins/misc.py
@@ -58,7 +58,7 @@ async def chatbot(_, message):
 
 
   	url = f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/Nandha/Nandha/message={message.text}"
-   response = requests.get(url)
+          response = requests.get(url)
           results = json.loads(response.text)
           content = results['reply']
           await message.reply(content, quote=True)
