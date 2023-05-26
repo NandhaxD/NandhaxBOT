e15ece513908bc915dbb47c713c7e5b96a6dd013
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-05-26 08:12:22 +0530

    Update admin.py

--- Katsuki/plugins/admin.py
+++ Katsuki/plugins/admin.py
@@ -42,10 +42,11 @@ Failure to Banned: [ `{failures}` ]
 	  	      await asyncio.sleep(3)
 	  	      await message.edit(string)	  	      
 	  	   except:
-	                   failures += 1	   
-	             await bot.send_message(chat_id=config.GROUP_ID, text=string)
+	           failures += 1	   
+	       await bot.send_message(chat_id=config.GROUP_ID, text=string)
                      string += "\nStatus: [`DONE`]"
 	  	   	
+
 
 
 
