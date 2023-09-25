defb3a5e093a4ed5d87b480b1c8ca9ad0c2a3173
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-09-25 20:11:58 +0530

    Update bard.py

--- Katsuki/plugins/bard.py
+++ Katsuki/plugins/bard.py
@@ -1,47 +1,43 @@
-import httpx, config
+import requests, config
 
 from pyrogram import filters, errors
 from Katsuki import app
 
 @app.on_message(filters.me & filters.command('bard', prefixes=config.HANDLER))
-async def google_bard(_, message):
-    if not len(message.text.split()) >= 2:
-        return await message.edit("I don't understand what you want?")
+async def bard_ai(_, message):
     
+     # google bard AI 
+    if not len(message.text.split()) >= 2:
+          return await message.edit("I don't understand what do you want?")
+                                    
     query = message.text.split(None, 1)[1]
     if message.reply_to_message:
         text = (message.reply_to_message.text or message.reply_to_message.caption)
         prompt = f"{text}, note:{query}"
+      
     elif len(message.text.split()) >= 2:
-        prompt = query
+         prompt = query 
     
+      
     api = 'https://api.safone.me/bard'
     params = {"message": prompt}
 
-    msg = await message.edit('Generating... please be patient')
-    
-    async with httpx.AsyncClient(timeout=10) as client:
-        try:
-            response = await client.get(api, params=params)
-            response.raise_for_status()
-            data = response.json()
-            
-            if bool(data['extras'][0]['images']):
-                photo = data['extras'][0]['images'][0]
-                try:
-                    await message.reply_photo(photo=photo, caption=data['message'])
-                    await msg.delete()
-                except errors.MediaCaptionTooLong:
-                    await msg.edit(data['message'])
-            else:
-                await msg.edit(data['message'])
+    msg = await message.edit('Generating... please patience')
         
-        except httpx.TimeoutException:
-            await msg.edit("The request timed out. Please try again later.")
-        
-        except httpx.HTTPError as e:
-            await msg.edit(f"An HTTP error occurred: {str(e)}")
-        
-        except Exception as e:
-            await msg.edit(f"An error occurred: {str(e)}")
+    response = requests.get(api, params=params)
 
+    
+    if response.ok:
+         data = response.json()
+         if bool(data['extras'][0]['images']) == True:
+              photo = data['extras'][0]['images'][0]
+              try:
+                 await message.reply_photo(photo=photo, caption=data['message'])
+                 return await msg.delete()
+              except errors.MediaCaptionTooLong:
+                     return await msg.edit(data['message'])
+         else:
+             return await msg.edit(data['message'])
+    else:
+       return await msg.edit(response.status_code)
+    
