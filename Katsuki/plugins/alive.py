ac923e284d7c4fea9b702cb244524e1bf4ddcbb1
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-09-28 22:04:30 +0530

    Update alive.py

--- Katsuki/plugins/alive.py
+++ Katsuki/plugins/alive.py
@@ -2,7 +2,7 @@
 Copyright © [2023-2024] @NandhaBots. All rights reserved. Reproduction, modification, distribution, or republication of this file without prior written permission from @NandhaBots is strictly prohibited. The Katsuki Telegram user bot has been developed with the Pyrogram library and utilizing Python programming language, making it a safe and secure option for users. Unauthorized use of this bot or any part of it may result in legal action. This project is owned by @Nandha, and any unauthorized use or distribution of this bot is strictly prohibited.
 """
 
-import config, asyncio
+import config, asyncio, image
 from pyrogram import filters
 from Katsuki import app
 
@@ -23,6 +23,8 @@ async def alive(_, message):
     await asyncio.sleep(2)
     await msg.edit('0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣')
     await asyncio.sleep(2)
-    await msg.edit('alive!')
+    await message.repy_photo(image.alive, caption=string.alive)
+    await msg.delete()
+    
     
   
