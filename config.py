66c50170d02367cbd9af1b6203ae9300ad94b6b5
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-09-28 19:49:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os, sys
 
 """ CHANGE TO FALSE IF YOU DON'T WANNA ADD VARIABLES IN HOSTING SITE """
 
-ENV = os.getenv("ENV", True)
+ENV = os.getenv("ENV", False)
 
 
 if ENV:    
@@ -24,13 +24,13 @@ if ENV:
            sys.exit()
            print(f"THE {var} ENVIRONMENT VARIABLE IS MISSING.")
 else:
-   API_ID = None #int() method | get from my.telegram.org
-   API_HASH = None #str() method | get from my.telegram.org
-   BOT_TOKEN = None #str() method | get via @botfather
-   SESSION = None #str() method | use replit or bots to get your Pyro session 
-   GROUP_ID = None #int() menthod | your group id
-   DB_URL = None # get from mongodb.com
-   OWNER_ID = None # fill ur user id
+   API_ID = 13257951 #int() method | get from my.telegram.org
+   API_HASH = "d8ea642aedb736d40035bc05f0cfd477" #str() method | get from my.telegram.org
+   BOT_TOKEN = "6059605779:AAFQpCCtPW2Xehr5Ce-Ppv2FgEmGXxY35nE" #str() method | get via @botfather
+   SESSION = "BQBZKrw0uAaWK1JNYJRV_0bAK-Dv-bvMfQWihxRVyj2MAVbRX6pAzz6TjhMWrsJyBRxnCx_NXC0yynjNu_cY5dCSEmdBB92YlEKq1_MjK49_kiULD5D3lJ7Np6sIG2RDSEmqJLy0NNrZoUCn8auYq3f6FyoFLsWb7rCcrm3RjMuehP-HY7bW31U1sWaMdMEmugCD8GrzLVDktLSLI2MjVTGJoy9bH232QzrR3mpch5jGo7RCrc4xySOkxJ_uvi3F3b09xycaAKiYMACEgDWkIWILoFBOH0J8pSkaIBbGncohj41d11EmDVzMQ1gVUqRNQiSTeT1q-LcppbfEvM5E-Px0AAAAAVOC3-wA" #str() method | use replit or bots to get your Pyro session 
+   GROUP_ID = -1001717881477 #int() menthod | your group id
+   DB_URL = "mongodb+srv://nandhaxd:u7uHs7EhyuyLT5Kj@cluster0.80igexg.mongodb.net/" # get from mongodb.com
+   OWNER_ID = 5696053228 # fill ur user id
 
 
 """ DEFAULT VARIABLES """
