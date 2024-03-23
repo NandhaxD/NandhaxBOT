b646586d8c8b3f8c5b509c35ac3619a8b77ca31b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-24 01:42:21 +0530

    Update decorator.py

--- Katsuki/helpers/decorator.py
+++ Katsuki/helpers/decorator.py
@@ -13,16 +13,28 @@ from Katsuki import app
 """ CHECK YOUR ACC HAS ADMIN IN CHAT """ 
   
 
+
+
+async def is_admin(chat_id, user_id):
+      kk = await app.get_chat_member(chat_id, user_id)
+      admin = kk.status == enums.ChatMemberStatus.ADMINISTRATOR
+      owner = kk.status == enums.ChatMemberStatus.OWNER
+      if (admin or owner):
+           return True, kk
+      else:
+           return False, kk
+           
+
+  
 def admin_only(func): 
          async def wrapped(app:app, message:Message): 
              chat_id=message.chat.id 
              user_id=message.from_user.id 
              if message.chat.type==enums.ChatType.PRIVATE: 
-                 return await message.edit("[`THIS COMMAND ONLY FOR GROUP`]") 
-             check = await message.chat.get_member(user_id) 
-             is_admin = check.status==enums.ChatMemberStatus.ADMINISTRATOR 
-             if not is_admin:
-                 return await message.edit("[`YOU ARE NOT ADMIN`]")
+                 return await message.edit("This command only work in groups.") 
+             
+             if not is_admin(chat_id, user_id)[0]:
+                 return await message.edit("you're not admin.")
              return await func(app, message)                 
          return wrapped
 
@@ -36,13 +48,12 @@ def can_restrict_members(func):
              user_id=message.from_user.id 
              if message.chat.type==enums.ChatType.PRIVATE: 
                  return await message.edit("This command only work in gorups.") 
-             check = await message.chat.get_member(user_id) 
-             is_admin = check.status==enums.ChatMemberStatus.ADMINISTRATOR
-             if not is_admin:  
-                   return await message.edit("You're not admin")
-             elif check.privileges is None:
-                    return await message.reply("I can't check admin rights")
-             elif not check.privileges.can_restrict_members:
+             
+             if not is_admin(chat_id, user_id)[0]:  
+                   return await message.edit("You're not admin.")
+             elif not is_admin(chat_id, user_id)[1].privileges is None:
+                    return await message.reply("I can't check admin rights here.")
+             elif not is_admin(chat_id, user_id)[1].privileges.can_restrict_members:
                  return await message.edit("you can't ban users from here.")
              return await func(app, message)                 
          return wrapped 
