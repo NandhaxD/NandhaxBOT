d7a9f82abc0f15e6e52a0935d3e89729c9379a55
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-23 07:32:19 +0530

    Update misc.py

--- Katsuki/plugins/misc.py
+++ Katsuki/plugins/misc.py
@@ -22,7 +22,7 @@ import requests
 	         
 @app.on_message(filters.me & filters.command("help", prefixes=config.HANDLER))
 async def help_command(_, message):
-      BOT_USERNAME = await bot.get_me().username
+      BOT_USERNAME = (await bot.get_me()).username
       query = "help"
       result = await app.get_inline_bot_results(bot=BOT_USERNAME, query=query)
       await app.send_inline_bot_result(chat_id=message.chat.id, query_id=result.query_id, result_id=result.results[0].id, reply_to_message_id=message.id)
