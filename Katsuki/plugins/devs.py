4bb8f1387b63dea64c363c7859a48b60e44e29d1
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-24 16:51:05 +0530

    Update devs.py

--- Katsuki/plugins/devs.py
+++ Katsuki/plugins/devs.py
@@ -105,7 +105,7 @@ async def evaluate(app , message):
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        await aexec(cmd, app, message, reply)
+        await aexec(cmd, app, message, r)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
