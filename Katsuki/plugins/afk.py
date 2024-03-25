5d4a64cb113280eb61812daf6bf1f8ad35cd4c5d
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-25 07:55:44 +0530

    Create afk.py

---
+++ Katsuki/plugins/afk.py
@@ -0,0 +1,48 @@
+import config
+from Katsuki import app
+from pyrogram import filters, enums
+
+
+AFK = {'afk': False, 'reason': 'not Mentioned'} 
+
+
+@app.on_message(filters.me & filters.command('afk', prefixes=config.HANDLER))
+async def away_from_keyboard(_, message):
+     global AFK
+     
+     if len(message.command) <2:
+           reason = None
+     else:
+           reason = message.text.split(None, 1)[1]
+       
+     AFK['afk'] = True
+     AFK['reason'] = reason
+     return await message.reply('<b>You are now AFK!</b>')
+
+@app.on_message(filters.me, group=4)
+async def back_to_life(_, message):
+      global AFK
+      check = AFK['afk']
+      if check:
+           AFK['afk'] = False
+           return await message.reply("<b>Welcome back by Katsuki ❤️!</b>")
+
+@app.on_message((filters.reply|filters.text))
+                &~filters.group , group=2)  
+async def afk_check(_, message):
+
+        text = message.text
+        pattern = r'@nandha\b'
+        result = re.search(pattern, text) is not None
+        r = message.reply_to_message
+        IS_AFK = AFK['afk']
+        if ((r.from_user.id) == config.OWNER_ID or result)
+                   and IS_AFK):
+              reason = AFK['reason']
+              if reason is not None:
+                     return await message.reply(f'<pre>AFK: Away from keyboard</pre>\n<pre>r
+                     Reason:</pre><pre>{reason}</pre>', parse_mode=enums.ParseMode.HTML)
+              else:
+                   return await message.reply('<b>Offline! ❤️ </b>')
+             
+                
