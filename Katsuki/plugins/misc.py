ab5a0632277289a0b424cb89c8e285c70f291c13
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-26 06:55:13 +0530

    Update misc.py

--- Katsuki/plugins/misc.py
+++ Katsuki/plugins/misc.py
@@ -31,12 +31,12 @@ async def artificial_intelligent(_, message):
 	model = message.text.split()[0]
 	model = ai_models[model]
 	prompt = message.text.split(None, 1)[1]
-	api = f"https://tofu-node-apis.onrender.com/api/{model}/{quote(prompt, safe='')}"	
-	#try:		
-	response = requests.get(api).json()
-	ok = response.get('reply')		
-	#except Exception as e:
-		# return await reply.edit(f"<pre>Errors:</pre>{e}", parse_mode=enums.ParseMode.HTML)				
+	api = f"https://tofu-node-apis.onrender.com/api/{model}?/{quote(prompt, safe='')}"	
+	try:		
+	    response = requests.get(api).json()
+	    ok = response.get('reply')		
+	except Exception as e:
+		 return await reply.edit(f"<pre>Errors:</pre>{e}", parse_mode=enums.ParseMode.HTML)				
 	return await reply.edit(f'<pre>{model.upper()}:</pre>\n<pre>prompt: {prompt}</pre>\n <blockquote>{ok}</blockquote>', parse_mode=enums.ParseMode.HTML)
 		
          
