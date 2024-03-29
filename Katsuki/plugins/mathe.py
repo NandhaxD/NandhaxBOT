0f59910fbe916fa8b60ba0c0b87b976343b1c352
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-29 15:42:40 +0530

    Update mathe.py

--- Katsuki/plugins/mathe.py
+++ Katsuki/plugins/mathe.py
@@ -40,8 +40,8 @@ async def math_riddle(_, message):
            await m.reply_photo(
                photo='boom.jpg', 
                        caption=lang['riddle'].format(query, result))
-    except Exception as e:
+     except Exception as e:
             return await msg.edit(lang['error'].format(e))
-    os.remove("image.jpg")
-    os.remove("boom.jpg")
+     os.remove("image.jpg")
+     os.remove("boom.jpg")
      
