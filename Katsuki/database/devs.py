d0e7c8fff3361e0fa512ed3cf272fdc7d03f9148
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-03-30 06:34:34 +0530

    Update and rename afk_db.py to devs.py

---
+++ Katsuki/database/devs.py
@@ -0,0 +1,29 @@
+
+from Katsuki import DATABASE
+
+db = DATABASE['DEV_USERS']
+
+
+
+async def get_users(): > list:
+     list = [x['user_id'] for x in db.find()]
+     return list
+
+async def add_user(user_id: int):
+    list = await get_users()
+    if not user_id in list:
+          user = {'user_id': user_id}
+          db.insert_one(user)
+
+
+
+async def remove_user(user_id: int):
+      user = {'user_id': user_id}
+      user = db.find_one(user)
+      if user:
+         db.delete_one(user)
+      return True
+      
+  
+  
+
