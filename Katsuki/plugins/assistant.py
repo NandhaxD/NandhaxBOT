2de804780935640261ab8dfa1d8bd2b66e753b4b
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-01 03:19:35 +0530

    Update assistant.py

--- Katsuki/plugins/assistant.py
+++ Katsuki/plugins/assistant.py
@@ -6,7 +6,9 @@ Copyright © [2023-2024] @NandhaBots. All rights reserved. Reproduction, modific
 
 
 import config
-import asyncio, random 
+import asyncio, random
+import requests
+import base64
 
 from pyrogram import filters, enums
 from Katsuki import bot ,app, lang
@@ -39,4 +41,73 @@ async def start(_, message):
           caption=lang['bot_start_01'].format(mention, applive,botlive), quote=True, reply_markup=BUTTON)
      await asyncio.sleep(10)
      SPAM.remove(user_id)
+
+
+
+data = {
+  "type": "quote",
+  "format": "webp",
+  "backgroundColor": None,
+  "width": 512,
+  "height": 768,
+  "scale": 2,
+  "messages": [
+    {
+      "entities": [],
+      "avatar": True,
+      "from": {
+        "id": 1,
+        "name": None,
+        "photo": {
+          "url": None
+        }
+      },
+      "text": None,
+      "replyMessage": {}
+    }
+  ]
+}
+
+
+@bot.on_message(filters.command('q', prefixes=config.HANDLER))
+async def quote(_, message):
+     reply = message.reply_to_message
+     if reply:
+           if not len(message.text.split()) < 2:
+                 bg_code = message.text.split()[1]
+           else:
+                 bg_code = "332255"
+           text = reply.text if reply.text else None
+           if text is None:
+                 return await message.edit('You can only do with text.')
+           photo_id = reply.from_user.photo.big_file_id if reply.from_user.photo else "AgACAgUAAxkDAAECAW9mCc9TJA-yyVyZ12RsrE2MAyr1YAACx7oxGz2WUVRbAAGQzuS-v5UACAEAAwIAA3gABx4E"
+           username = reply.from_user.first_name
+           avatar_url = await grap(await app.download_media(photo_id))
+          
+     else:
+         if not len(message.text.split()) < 2:
+               try:
+                  bg_code = message.text.split(message.text.split()[0])[1].split('|')[0]
+                  is_bg_code = True
+               except:
+                    is_bg_code = False
+                    bg_code = "332255"
+               text = message.text.split(None, 1)[1] if is_bg_code is False else message.text.split(message.text.split()[0])[1].split('|')[1]
+               username = message.from_user.first_name
+               photo_id = message.from_user.photo.big_file_id if message.from_user.photo else "AgACAgUAAxkDAAECAW9mCc9TJA-yyVyZ12RsrE2MAyr1YAACx7oxGz2WUVRbAAGQzuS-v5UACAEAAwIAA3gABx4E"
+               avatar_url = await grap(await app.download_media(photo_id))
+
+     data['backgroundColor'] = bg_code
+     data['messages'][0]['from']['name'] = username
+     data['messages'][0]['from']['photo']['url'] = avatar_url
+     data['messages'][0]['text'] = text
+     response = requests.post('https://bot.lyo.su/quote/generate', json=json).json()
+     buffer = base64.b64decode(response['result']['image'].encode('utf-8'))
+     name = f"{username}.webp"
+     open(name, 'wb').write(buffer)
+     await message.reply_sticker(sticker=name)
+     return await message.delete()
+     
+     
+
      
