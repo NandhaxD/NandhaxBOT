6f007a49717290b7dda5ed7879c92f3425eef252
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-04 20:09:47 +0530

    Update start.py

--- Katsuki/plugins/assistant/start.py
+++ Katsuki/plugins/assistant/start.py
@@ -1,1 +1,36 @@
 
+import config
+import random
+import requests 
+from pyrogram import filters, enums
+from Katsuki import bot ,app, lang
+from Katsuki.helpers.help_func import emoji_convert, anime_gif_key, get_anime_gif
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+SPAM = []
+
+@bot.on_message(filters.command("start"))
+async def start(_, message):
+     user_id = message.from_user.id
+
+     key = random.choice(anime_gif_key)
+     animation = await get_anime_gif(key=key)
+     
+     if user_id in SPAM:
+         return await message.reply(lang['bot_start_02'])
+     botlive = await emoji_convert(bot.is_connected)
+     applive = await emoji_convert(app.is_connected)
+     name = config.NAME
+     id = config.OWNER_ID
+     SPAM.append(user_id)
+     await message.forward(config.OWNER_ID)
+     mention = f"[{name}](tg://user?id={id})"
+     BUTTON=InlineKeyboardMarkup([[
+     InlineKeyboardButton("Source ⬅️", url=config.SOURCE),InlineKeyboardButton("GROUP ⬅️", url=config.GROUP_LINK)]])
+     await message.reply_animation(
+          animation=animation,
+          caption=lang['bot_start_01'].format(mention, applive,botlive), quote=True, reply_markup=BUTTON)
+     await asyncio.sleep(10)
+     SPAM.remove(user_id)
+  
