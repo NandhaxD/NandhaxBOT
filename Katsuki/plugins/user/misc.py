461407509b0430d0cdd3445ecdac36257e39707f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-05 03:05:48 +0530

    Update misc.py

--- Katsuki/plugins/user/misc.py
+++ Katsuki/plugins/user/misc.py
@@ -80,14 +80,7 @@ async def artificial_intelligent(_, message):
 	return await reply.edit(lang['AI'].format(model.upper(), prompt, ok), parse_mode=enums.ParseMode.HTML)
 	
          
-	         
-@app.on_message(filters.me & filters.command("help", prefixes=config.HANDLER))
-async def help_command(_, message):
-      BOT_USERNAME = (await bot.get_me()).username
-      query = "help"
-      result = await app.get_inline_bot_results(bot=BOT_USERNAME, query=query)
-      await app.send_inline_bot_result(chat_id=message.chat.id, query_id=result.query_id, result_id=result.results[0].id, reply_to_message_id=message.id)
-      await message.delete()
+
 
 @app.on_message(filters.me & filters.command("git",prefixes=config.HANDLER)) 
 async def github(_, message): 
