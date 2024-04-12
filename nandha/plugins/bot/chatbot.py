3a661872d6acead0587db051937bddfc29454cc3
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-12 22:54:59 +0530

    Update chatbot.py

--- nandha/plugins/bot/chatbot.py
+++ nandha/plugins/bot/chatbot.py
@@ -8,19 +8,14 @@ from nandha import bot
 from pyrogram import filters, enums
 
 
-is_chatbot = False
-
 api = 'https://tofu-node-apis.onrender.com/api/charai?charID=gkLVbtXVSkljrRjnkzOowUbfXntEOFB2iWtu3xBj19A&q={}'
 
-@bot.on_message(filters.reply & filters.text & filters.chat(-1001717881477) & ~filters.bot)
+@bot.on_message(filters.reply & filters.text & filters.chat(config.GROUP_ID) & ~filters.bot)
 async def reply_chatbot(_, message):
+
+      # This chatbot module will work only in your group chat. CUSTOMIZED AI
       
-      chat_id = message.chat.id
-      
-      if is_chatbot is False:
-          return
-            
-      else:    
+          chat_id = message.chat.id    
           reply = message.reply_to_message
           if reply.from_user and reply.from_user.id == config.BOT_ID:
                name = message.from_user.first_name
@@ -33,35 +28,7 @@ async def reply_chatbot(_, message):
                     success = response['reply']
                     if success:
                         await bot.send_chat_action(chat_id, enums.ChatAction.TYPING)
-                        await message.reply(success)
+                        return await message.reply(success)
                except:
-                   return 
+                   return message.reply('What?')
         
-
-
-
-@bot.on_message(filters.command('chatbot', prefixes=config.HANDLER))
-async def chatbot(_, message):
-    global is_chatbot
-    if len(message.text.split()) < 2:
-        return await message.reply(
-          'Example: !chatbot on|off')
-    else:
-         query = message.text.split()[1].lower()
-         if query == 'on':
-             is_chatbot = True
-             return await message.reply(
-               'Successfully turn onend chatbot in the chat group.')
-         elif query == 'off':
-               is_chatbot = False
-               return await message.reply(
-                  'Successfully turn offend chatbot in the chat group.')
-         else:
-              return await message.reply(
-          'Example: !chatbot on|off')
-              
-
-
-
-
-           
