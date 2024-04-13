a8709acae78c74ea64f74301a9596423d609a825
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-13 11:45:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ENV = os.getenv("ENV", True)
 if ENV:    
    API_ID = os.getenv("API_ID", 13257951)
    API_HASH = os.getenv("API_HASH", "d8ea642aedb736d40035bc05f0cfd477")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "6952927982:AAFIAAc8cm39XZz9yeMGN6qXEgLMR1rnIWY")
+   BOT_TOKEN = os.getenv("BOT_TOKEN")
    SESSION = os.getenv("SESSION")
    GROUP_ID = os.getenv("GROUP_ID", -1001717881477)
    OWNER_ID = os.getenv('OWNER_ID', 5696053228)
