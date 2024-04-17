2c0e54f0221b2c97e5845862f0e15fa9003adc40
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-17 15:07:35 +0530

    Create misc.py

---
+++ nandha/helpers/misc.py
@@ -0,0 +1,20 @@
+
+
+a
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+
+
+
+
+
+async def article(name: str, article: str, thumb_url: str , keyboard: str = None):     
+     results=[
+         InlineQueryResultArticle(
+                   name,
+                     InputTextMessageContent(
+                message_text=article,
+                               disable_web_page_preview=True), 
+             thumb_url=thumb_url, 
+             reply_markup=keyboard)
+     ]
+     return results
