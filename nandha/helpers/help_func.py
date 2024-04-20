09ff42cc829f7cf49b270251bed0b9922d2691e2
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-21 00:09:10 +0530

    Update help_func.py

--- nandha/helpers/help_func.py
+++ nandha/helpers/help_func.py
@@ -19,6 +19,13 @@ from nandha import session
 
 
 
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
 
 async def get(url: str, *args, **kwargs):
     async with session.get(url, *args, **kwargs) as resp:
