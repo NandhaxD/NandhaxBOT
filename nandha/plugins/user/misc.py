c8fb0dec548ce68183fca0c3e750d17eb3f0c06d
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-21 10:58:29 +0530

    Update misc.py

--- nandha/plugins/user/misc.py
+++ nandha/plugins/user/misc.py
@@ -41,15 +41,17 @@ async def ping(_, message):
 @app.on_message(filters.me & filters.command('cb', prefixes=config.PREFIXES))
 async def carbon(_, message):
 	reply = message.reply_to_message
-	if not reply
+	if not reply:
              msg = await message.edit(lang['reply_to'])
 	     await asyncio.sleep(10)
 	     return await msg.delete()
 	else:
 	    try:
 	      query = message.text.split(None, 1)[1]
+	      msg = await message.edit(lang['alyz'])
 	      image = await make_carbon(query)
 	      await message.reply_photo(photo=image)
+	      await msg.delete()    
 	    except Exception as e:
 		     return await message.edit(lang['error'].format(e))
 
