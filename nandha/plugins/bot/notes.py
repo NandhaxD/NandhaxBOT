c71947bae51f64e70ed4856354323ce439799a59
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-23 15:54:54 +0530

    Create notes.py

---
+++ nandha/plugins/bot/notes.py
@@ -0,0 +1,26 @@
+from nandha import bot
+from nandha.helpers.help_func import get_note_deatils
+from pyrogram import filters
+
+right_format = 'Eg: `/save {notename} reply to text/media` or give text like `{notename} @nandha`'
+
+@bot.on_message(filters.command('save'))
+async def save_note(_, message):
+     
+     reply = message.reply_to_message
+  
+     if not reply:
+         if len(message.text.split()) <= 2
+             return await message.reply(
+                  right_format
+             )
+     elif reply:
+          if len(message.text.split()) < 2:
+              return await message.reply(
+                 right_format
+              )
+    
+      note = await get_note_deatils(message)
+      return await message.reply(note)
+
+        
