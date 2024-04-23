393ca62b052748aa67a0e28b6a83a77706b4d964
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-23 23:54:42 +0530

    Update notes.py

--- nandha/plugins/bot/notes.py
+++ nandha/plugins/bot/notes.py
@@ -1,12 +1,90 @@
 
+import config
+
 from nandha import bot
 from nandha.helpers.help_func import get_note_deatils
-from nandha.database.notes import get_notes_list, add_note
+from nandha.database.notes import get_notes_list, add_note, get_note
 from pyrogram import filters
 
 right_format = 'Eg: `/save {notename} reply to text/media` or give text like `{notename} @nandha`'
-exists = 'Note name already exist. delete and try again.'
-added = 'Added `#{}`'
+exists = '**Note name already exist. delete and try again.**'
+added = 'Added!:`{}`\nGet the note using `/get notename`'
+
+get_note_eg = 'Example:\n`/get {notename}`'
+
+
+@bot.on_message(filters.command('get', prefixes=config.PREFIXES))
+async def get_note(_, message):
+      chat_id = message.chat.id
+      reply = message.reply_to_message
+     
+      if len(message.text.split()) < 2:
+            return await message.reply(
+                 get_note_eg
+            )   
+      else:
+          note_name = message.text.split()[1]
+          note = await get_note(
+               chat_id=chat_id,
+               name=note_name
+          )
+          note = notes[0]
+          type = note.get('type', '')
+          file_id = note.get('file_id', '')
+          caption = note.get('caption', None)
+          text = note.get('text', '')
+
+
+          if type == '#STICKER':
+               reply_func = reply.reply_sticker if reply else message.reply_sticker
+               return await reply_func(file_id)
+          elif type == '#PHOTO':
+               reply_func = reply.reply_photo if reply else message.reply_photo
+               return await reply_func(
+                    photo=file_id, 
+                    caption=caption
+               )
+          elif type == '#TEXT':
+                reply_func = reply.text if reply else message.reply_text
+                return reply_func(
+                     text=text
+                )
+          elif type == '#ANIMATION':
+                reply_func = reply.animation if reply else message.reply_animation
+                return reply_func(
+                     animation=file_id, 
+                     caption=caption
+                )
+          elif type == '#DOCUMENT':
+                reply_func = reply.document if reply else message.reply_document
+                return reply_func(
+                     document=file_id, 
+                     caption=caption
+                )
+          elif type == '#VIDEO':
+                reply_func = reply.video if reply else message.reply_video
+                return reply_func(
+                     video=file_id, 
+                     caption=caption
+                )
+          
+
+          
+
+     
+     
+
+ 
+
+      
+      
+
+
+
+
+
+
+
 
 @bot.on_message(filters.command('save'))
 async def save_note(_, message):
