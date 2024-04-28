3ed75b0290a7d160f106671948bd063470db2898
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-29 00:38:57 +0530

    Update notes.py

--- nandha/database/notes.py
+++ nandha/database/notes.py
@@ -7,11 +7,24 @@ async def get_chats_list() -> list:
     chat_ids = [ chats['chat_id'] for chats in db.find() ]
     return chat_ids
 
-
+async def get_notes_list(chat_id: int):
+    chat = db.find_one({'chat_id': chat_id})
+    if chat:
+        notes = [ note['name'] for note in chat['notes'] ]
+        return notes
+        
 
 async def get_note(name, chat_id):
-    col = db.find_one({'chat_id': chat_id})
-    if not col is None:
+    col = db.find_one({'chat_id': chat_id})    
+    length = len(await get_notes_list(chat_id)) if await get_notes_list(chat_id) is not None else 0
+    available_num = [i + 1 for i in range(length)]
+    if (name.isdigit()) and (name in available_num):
+          notes = col['notes']
+          note = notes[name-1]
+          return note
+        
+    else:
+       if col:
           notes = col.get('notes', [])
           note = [ note for note in notes if note['name'] == name ]
           if len(note) == 1:
@@ -41,11 +54,7 @@ async def delete_note(name, chat_id):
         return False
 
 
-async def get_notes_list(chat_id: int):
-    chat = db.find_one({'chat_id': chat_id})
-    if chat:
-        notes = [ note['name'] for note in chat['notes'] ]
-        return notes
+
 
 async def add_note(chat_id, data):
       notes = db.find_one({'chat_id': chat_id})
