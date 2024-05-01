bbb9d05d4b1bf2b7468a0ed6bd3ff6c8089e250c
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-01 09:47:56 +0530

    Update bot_main.py

--- nandha/plugins/bot/bot_main.py
+++ nandha/plugins/bot/bot_main.py
@@ -3,7 +3,7 @@ import random
 import asyncio 
 import requests 
 from pyrogram import filters, enums
-from nandha import bot ,app, lang
+from nandha import bot ,app, lang, DATABASE
 from nandha.helpers.help_func import emoji_convert, anime_gif_key, get_anime_gif
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -13,6 +13,24 @@ SPAM = []
 @bot.on_message(filters.command("start"))
 async def start(_, message):
      user_id = message.from_user.id
+
+     if len(message.text.split()) == 2 and message.text.split()[1].startswith('file') and message.chat.type == enums.ChatType.PRIVATE:
+            token = message.text.split(':')[1]
+            db = DATABASE['LINK_TO_FILE']
+            user = db.find_one({'user_id': user_id})            
+            if user:
+               ignore = ['_id', 'user_id']
+               user_tokens = [ token for token in db.find_one(user_json) if token not in ignore]
+               if token in user_token:
+                    file_ids = user[token]
+                    
+                    for file_id in file_ids:
+                        await bot.send_document(
+                             chat_id, document=file_id, quote=True
+                        )
+                    return await message.reply(
+                         f'**Successfully uploaded {len(file_ids)} thank you for using Me.**')
+          
 
      key = random.choice(anime_gif_key)
      animation = await get_anime_gif(key=key)
