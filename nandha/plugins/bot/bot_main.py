d140b7a4d35f515af9925b365608e94103227e88
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-01 11:33:18 +0530

    Update bot_main.py

--- nandha/plugins/bot/bot_main.py
+++ nandha/plugins/bot/bot_main.py
@@ -15,13 +15,11 @@ async def start(_, message):
      user_id = message.from_user.id
 
      if len(message.text.split()) == 2 and message.text.split(None,1)[1].startswith('getfile') and message.chat.type == enums.ChatType.PRIVATE:
-            await message.reply(message.text)
-          
             try:
               token = message.text.split(None, 1)[1].split('-')[-1]
             except Exception as e:
                  return await message.reply(
-                      'Your method is invalid try t.me/{botusername}?start=file#{token} 🤔.')
+                      'Your method is invalid try t.me/{botusername}?start=getfile-{token} 🤔.')
                  
             db = DATABASE['LINK_TO_FILE']
             user = db.find_one({'user_id': user_id})            
