e59d00a77cf6fdc7a37dabc8eaf26b407b5c3b63
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-01 09:11:19 +0530

    Update linktofile.py

--- nandha/plugins/bot/linktofile.py
+++ nandha/plugins/bot/linktofile.py
@@ -2,6 +2,9 @@
 
 
 import base64
+import secrets
+
+
 
 
 from nandha import bot, DATABASE
@@ -9,6 +12,11 @@ from nandha.helpers.decorator import devs_only
 
 from pyrogram import filters, types
 
+
+def gen_token():
+    random_hex = secrets.token_hex(16)
+    return random_hex
+    
 def encode(string: str):
     encoded_bytes = base64.b64encode(string.encode('utf-8'))
     return encoded_bytes.decode('utf-8')
@@ -30,26 +38,36 @@ async def Getlink(_, message):
        if document:
             file_id = document.file_id
             user_json = {'user_id': user_id}
-            if db.find_one(user_json):
-          
-                 token = encode(file_id)
-                 db.update_one(
-                   user_json, {'$push': {token: file_id}})
+            if db.find_one(user_json) and len(message.text.split()) == 2:
+                 token = message.text.split(None, 1)[1]
+                 ignore = ['_id', 'user_id']
+                 user_tokens = [ token for token in db.find_one(user_json) if token not in ignore]
+                 if token in user_tokens:
+                     db.update_one(
+                        user_json, {'$push': {token: file_id}})
                  
-                 return await message.reply(
-                   '**Successfully added!**',
+                     return await message.reply(
+                         '**Successfully file added in token.**',
                    reply_markup=types.InlineKeyboardMarkup(
-                       [[types.InlineKeyboardButton('click here', url=link.format(id))]]
+                       [[types.InlineKeyboardButton('click here', url=link.format(token))]]
                    ))
+                 else:
+                     return await message.reply(
+                         'Sorry. itz not a valid token 🤔')
             else:
-                token = encode(file_id)
+                token = gen_token
                 db.update_one(
                    {'user_id': user_id},
                    {'$set': {token: [file_id]}},
                    upsert=True
                 )                
                 return await message.reply(
-                   '**Successfully added!**',
+                   '**Successfully new token generated and added file.**',
                     reply_markup=types.InlineKeyboardMarkup(
-                        [[types.InlineKeyboardButton('click here', url=link.format(id))]]
+                        [[types.InlineKeyboardButton('click here', url=link.format(token))]]
                     ))
+       else:
+          return await message.reply(
+       'Reply to the document file.'
+   )
+                
