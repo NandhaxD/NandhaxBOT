6c505e175bdf60b8617c159ac7fa2f9623cf0b29
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-04 22:27:26 +0530

    Update module.py

--- nandha/plugins/bot/module.py
+++ nandha/plugins/bot/module.py
@@ -3,20 +3,23 @@
 from nandha import bot, lang
 from nandha.helpers.help_func import get as async_get
 from urllib.parse import quote
-from pyrogram import filters
+from pyrogram import filters, enums
 
 
 
 @bot.on_message(filters.command(['bard','gpt', 'palm'], prefixes=""))
 async def artificial_intelligent(_, message):
+
+	reply = message.reply_to_message
 	
 	if len(message.command) <2:
 		return await message.reply(lang['query'])
-        
-	reply = await message.reply(lang['thinking'])
+
+	reply_func = reply.reply_text if reply else message.reply_text
+	reply = await reply_func(lang['thinking'])
 	model = message.text.split()[0]
 	prompt = message.text.split(None, 1)[1]
-	api = f"https://nandha-api.onrender.com/ai/{model}/{quote(prompt, safe='')}"	
+	api = f"https://nandha-api.onrender.com/ai/{model}/{quote(prompt)}"	
 	try:		
 	    response = await async_get(api)
 	    ok = response.get('content')		
