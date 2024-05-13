de0eb7e5de95188fbc7fe2ad16b42fba80783bd5
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-14 01:03:43 +0530

    Update welcome.py

--- nandha/plugins/bot/welcome.py
+++ nandha/plugins/bot/welcome.py
@@ -86,17 +86,14 @@ async def welcome(_, update):
      chat_id = update.chat.id
      user_id = update.from_user.id
      
-     is_welcome = await check_welcome(chat_id)
+     welcome_users = await check_welcome(chat_id)
 
      
-    not update.old_chat_member
-    or  is ChatMemberStatus.BANNED 
- 
      if (
           not update.old_chat_member           
           and update.new_chat_member
           and not update.new_chat_member.user.is_bot
-          and is_welcome 
+          and welcome_users
      ):
               
            mention = update.new_chat_member.user.mention()   
