4a07094289998b006fa5526a6aa7de3da91da2bc
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-13 13:49:16 +0530

    Update welcome.py

--- nandha/plugins/bot/welcome.py
+++ nandha/plugins/bot/welcome.py
@@ -5,6 +5,11 @@ from nandha import bot
 from pyrogram import filters
 
 @bot.on_chat_member_updated(group=4)
-async def welcome(_, message):
-     if message.new_chat_member:
-           await message.reply(message.new_chat_member)
+async def welcome(_, update):
+     chat_id = update.chat.id
+     user_id = update.from_user.id
+     if update.new_chat_member :
+           #photo, token, alt = await make_captcha(user_id, chat_id)
+          
+           await bot.send_message(
+                chat_id=chat_id, text='Hi,'+update.new_chat_member.user.mention())
