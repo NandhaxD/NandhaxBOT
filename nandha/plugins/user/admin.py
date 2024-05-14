0ca75b00b7483a76388bc1d4ae3d691314c1cd70
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 01:01:40 +0530

    Update admin.py

--- nandha/plugins/user/admin.py
+++ nandha/plugins/user/admin.py
@@ -15,14 +15,21 @@ import config
 @app.on_message(config.command('del'))
 @admin_rights(bot, 'can_delete_messages')
 async def delete_msg(_, message):
+
+    chat_id = message.chat.id
+    
     reply = message.reply_to_message
     if reply:
          await reply.delete()
          await message.delete()
     else:
       return await send_auto_del_msg(
-           bot,  chat_id, 
-          '`Reply message to delete.`', message.id,  5)
+           client=bot, 
+           method='message', 
+           chat_id=chat_id, 
+           text='`Reply message to delete.`',
+           reply_to_message_id=message.id, 
+           time=5)
       
 
      
