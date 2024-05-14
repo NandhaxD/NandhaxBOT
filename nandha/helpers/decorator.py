bf06fa0ee0c44efb7a5655c7db83e10ae2863504
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 00:27:43 +0530

    Update decorator.py
    Fixed and improved

--- nandha/helpers/decorator.py
+++ nandha/helpers/decorator.py
@@ -3,11 +3,11 @@
 Copyright © [2023-2024] @NandhaBots. All rights reserved. Reproduction, modification, distribution, or republication of this file without prior written permission from @NandhaBots is strictly prohibited. The Katsuki Telegram user bot has been developed with the Pyrogram library and utilizing Python programming language, making it a safe and secure option for users. Unauthorized use of this bot or any part of it may result in legal action. This project is owned by @Nandha, and any unauthorized use or distribution of this bot is strictly prohibited.
 """
 
-import config
+
 from pyrogram import enums, filters
-from pyrogram.types import Message 
-from nandha import lang , bot, app
 from nandha.database.devs import get_users
+
+import config
 
 
 class String:
@@ -43,6 +43,10 @@ def devs_only(func):
 def admin_only(client):
      def decorator(func):
          async def wrapped(_, message):
+
+             if message.chat.type == enums.ChatType.PRIVATE:
+                        return await func(_, message)
+                 
              chat_id = message.chat.id
              user_id = message.from_user.id
              client_info = await client.get_me()
@@ -66,6 +70,10 @@ def admin_only(client):
 def admin_rights(client, premission):
        def decorator(func):
              async def wrapped(_, message):
+
+                  if message.chat.type == enums.ChatType.PRIVATE:
+                        return await func(_, message)
+                      
                   chat_id = message.chat.id
                   user_id = message.from_user.id
                   client_info = await client.get_me()
@@ -87,9 +95,9 @@ def admin_rights(client, premission):
                       if privileges_dict.get(premission):
                             await func(_, message)
                       else:
-                          return await message.reply(f'`You are missing the [{premission}] required premission.`')
+                          return await message.reply(String.not_permission(who='You', premission=premission))
                   else:
-                      return await message.reply('You are not admin.')
+                      return await message.reply(String.not_admin(who='You'))
              return wrapped
        return decorator
 
