ddaae4af8c22e8e911cb081682bfe3c586c2ae4e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 00:54:36 +0530

    Create function.py
    Added send_auto_del_msg

---
+++ nandha/helpers/function.py
@@ -0,0 +1,22 @@
+
+import asyncio
+
+
+async def send_auto_del_msg(client, media, chat_id: int, text, reply_to_message_id: int, time: int):
+    # await send_auto_del_msg(bot, 'message', chat.id, 'ok', None, 5)
+  
+    method_name = f'send_{media}'
+    if hasattr(client, method_name):
+        send_method = getattr(client, method_name)
+        if media == 'message':
+            msg = await send_method(chat_id=chat_id, text=text, reply_to_message_id=reply_to_message_id)
+            await asyncio.sleep(time)
+            await msg.delete()
+        else:
+            msg = await send_method(chat_id=chat_id, caption=text, reply_to_message_id=reply_to_message_id) 
+            await asyncio.sleep(time)
+            await msg.delete()
+    else:
+        return False
+
+    
