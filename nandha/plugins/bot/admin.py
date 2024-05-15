fa8b5c9f28acec27a1aac1e0b98599de9982a8da
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 15:16:40 +0530

    Update admin.py

--- nandha/plugins/bot/admin.py
+++ nandha/plugins/bot/admin.py
@@ -5,8 +5,11 @@ from nandha.plugins.user.admin import (
 delete_msg 
 )
 
+from pyrogram import filters
 from pyrogram.handlers import MessageHandler
+import config
 
 
-bot.add_handler(delete_msg)
+
+bot.add_handler(delete_msg, config.command('del'))
 
