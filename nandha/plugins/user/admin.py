7067ce9465c234a46e71ba1602738f251a309c35
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-15 15:01:07 +0530

    Update admin.py

--- nandha/plugins/user/admin.py
+++ nandha/plugins/user/admin.py
@@ -13,7 +13,6 @@ import config
 
 
 @app.on_message(filters.me & config.command('del'))
-@bot.on_message(config.command('del'))
 @admin_rights('can_delete_messages')
 async def delete_msg(client, message):
 
