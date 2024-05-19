3b8327bf8e3c2a5853d2a7f68eed281ee6d6f1bd
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-19 13:37:08 +0530

    Update translate.py

--- nandha/plugins/bot/translate.py
+++ nandha/plugins/bot/translate.py
@@ -4,7 +4,7 @@ from urllib import parse
 from nandha import bot
 from pyrogram import filters
 
-import requests
+from nandha.helpers.trhozory import AsyncHozoryTranslator
 
 
 @bot.on_message(filters.command('tr'))
@@ -13,13 +13,14 @@ async def translate (_, message):
      reply = message.reply_to_message
      if len(message.text.split()) < 2:
          return await message.reply(usage)
-     if reply and reply.text or reply.caption:
+     if reply and (reply.text or reply.caption):
           text = reply.text or reply.caption
           code = message.text.split()[1]
           try:
-             api = requests.get(f'https://nandha-api.onrender.com/htranslate?text={parse.quote(text)}&code={code}').json()
-             tr_text = api['translated_text']
-             audio = api['voice_link']
+             hozory_engine = AsyncHozoryTranslator()
+             resp = hozory_engine.translate(text, code)
+             tr_text = resp['translated_text']
+             audio = resp['voice_link']
              try:
                  await message.reply_audio(
                        audio=audio, caption=(f'🧾 Code:\n`{code}`'
@@ -29,6 +30,6 @@ async def translate (_, message):
                        text=(f'🧾 Code:\n`{code}`'
                        f'\n🧾 Translate:\n{tr_text}'))
           except Exception as e:
-               return await message.reply('Error:\n', str(e))
+               return await message.reply(str(e))
      else:
         return await message.reply(usage)
