7d1328a9ddd5a1740037a5ee9b5910c8b58830d9
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-20 02:22:51 +0530

    Create symbol.py

---
+++ nandha/plugins/bot/symbol.py
@@ -0,0 +1,31 @@
+import json
+from pyrogram import types, filters
+from nandha import bot
+
+# Load the data from the JSON file
+with open("./nandha/helpers/database/symbols_data.json", "r", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+
+
+@bot.on_message(filters.command('symbols'))
+async def symbols(_, message):
+
+     chat_id = message.chat.id
+     buttons = []
+     for index, title in enumerate(list(data.keys()[:5])):
+          buttons.append(
+          title, types.InlineKeyboardButton(
+            title, callback_data=f'symbol:{index}'
+        )
+    )
+     columns_btn = [ [button] for button in buttons ]
+     reply_markup = types.InlineKeyboardMarkup(columns_btn)
+
+     return await bot.send_message(
+             chat_is=chat_id, text='Here the list of symbols, i hope you can find something awesome.',
+             reply_markup=reply_markup)
+
+
+             
+
