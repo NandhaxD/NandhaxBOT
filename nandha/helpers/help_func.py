003b98294be86ce529682b015b869b37c881f4a5
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-19 13:09:33 +0530

    Update help_func.py

--- nandha/helpers/help_func.py
+++ nandha/helpers/help_func.py
@@ -266,7 +266,7 @@ def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
     time_list = []
-    time_suffix_list = ["seconds", "minutes", "hours", "days"]
+    time_suffix_list = ["Seconds", "Minutes", "Hours", "Days"]
     while count < 4:
         count += 1
         remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
