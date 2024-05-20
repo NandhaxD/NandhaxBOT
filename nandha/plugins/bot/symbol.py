bd56400ab309c9d2cbbb997d845a0911cf186ad1
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-20 09:15:06 +0530

    Update symbol.py

--- nandha/plugins/bot/symbol.py
+++ nandha/plugins/bot/symbol.py
@@ -3,6 +3,7 @@ from pyrogram import types, filters
 from nandha import bot
 
 # Load the data from the JSON file
+
 with open("./nandha/helpers/database/symbols_data.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
@@ -13,9 +14,11 @@ async def symbols(_, message):
 
      chat_id = message.chat.id
      buttons = []
-     for index, title in enumerate(list(data.keys()[:5])):
+     for index, ( title, content) in enumerate(data.items()):
+          if index >= 4:
+               break
           buttons.append(
-          title, types.InlineKeyboardButton(
+            types.InlineKeyboardButton(
             title, callback_data=f'symbol:{index}'
         )
     )
