29eeb4890f9585a833dda79c328e6e3389f1dd3e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-02 23:06:58 +0530

    Update module.py

--- nandha/plugins/bot/module.py
+++ nandha/plugins/bot/module.py
@@ -2,6 +2,7 @@
 
 from nandha import bot, lang
 from nandha.helpers.help_func import get as async_get
+from nandha.helpers.function import extract_user, get_file_id 
 from urllib.parse import quote
 from pyrogram import filters, enums
 from bs4 import BeautifulSoup
@@ -160,23 +161,6 @@ async def stackoverflow(_, message):
 
 
 
-def get_file_id(msg: Message):
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            "sticker",
-        ):
-            if obj := getattr(msg, message_type):
-                setattr(obj, "message_type", message_type)
-                return obj
-
-
 @bot.on_message(filters.command(["id"]))
 async def showid(_, message):
     chat_type = message.chat.type.value
@@ -216,33 +200,6 @@ async def showid(_, message):
         await message.reply_text(_id, quote=True)
 
 
-
-def extract_user(message: Message) -> Union[int, str]:
-    """extracts the user from a message"""
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
-    user_id = None
-    user_first_name = None
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        user_first_name = message.reply_to_message.from_user.first_name
-
-    elif len(message.command) > 1:
-        if len(message.entities) > 1 and message.entities[1].type == "text_mention":
-            required_entity = message.entities[1]
-            user_id = required_entity.user.id
-            user_first_name = required_entity.user.first_name
-        else:
-            user_id = message.command[1]
-            # don't want to make a request -_-
-            user_first_name = user_id
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            pass
-    else:
-        user_id = message.from_user.id
-        user_first_name = message.from_user.first_name
-    return (user_id, user_first_name)
 
 
 @bot.on_message(filters.command(["info"]))
