20431c90d871370dae2b8990675aa46b8f937c09
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-02 23:06:13 +0530

    Update function.py

--- nandha/helpers/function.py
+++ nandha/helpers/function.py
@@ -2,21 +2,54 @@
 import asyncio
 
 
-async def send_auto_del_msg(client, method, chat_id: int, text, reply_to_message_id: int, time: int = 7):
-    # await send_auto_del_msg(bot, 'message', chat.id, 'ok', None, 5)
-  
-    method_name = f'send_{method}'
-    if hasattr(client, method_name):
-        send_method = getattr(client, method_name)
-        if method == 'message':
-            msg = await send_method(chat_id=chat_id, text=text, reply_to_message_id=reply_to_message_id)
-            await asyncio.sleep(time)
-            await msg.delete()
-        else:
-            msg = await send_method(chat_id=chat_id, caption=text, reply_to_message_id=reply_to_message_id) 
-            await asyncio.sleep(time)
-            await msg.delete()
-    else:
-        return False
+async def auto_del(msg, time: int = 7):        
+       await asyncio.sleep(time)
+       await msg.delete()
 
     
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker",
+        ):
+            if obj := getattr(msg, message_type):
+                setattr(obj, "message_type", message_type)
+                return obj
+
+
+def extract_user(message: Message) -> Union[int, str]:
+    """extracts the user from a message"""
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
+    user_id = None
+    user_first_name = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+    elif len(message.command) > 1:
+        if len(message.entities) > 1 and message.entities[1].type == "text_mention":
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            pass
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+    return (user_id, user_first_name)
+
+
