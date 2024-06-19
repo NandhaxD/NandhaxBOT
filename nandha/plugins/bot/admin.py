e7cf47d12fd8e5fa667db761edbb1918abc86840
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-19 15:56:28 +0530

    Update admin.py

--- nandha/plugins/bot/admin.py
+++ nandha/plugins/bot/admin.py
@@ -14,3 +14,12 @@ import config
 bot.add_handler(MessageHandler(delete_msg, filters.command('del')))
 bot.add_handler(MessageHandler(purge_msg, filters.command('purge')))
 
+
+
+__help__ = """
+Cmds:
+➩ /purge: delete multiply message.
+➩ /del: delete a specific message.
+"""
+
+___module__ = "Admin"
